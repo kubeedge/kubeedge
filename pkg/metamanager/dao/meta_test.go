c879f88071d6ebe70d50c02aab3c53b1e3aa5d79
Siddharth Bhadri <bhadri.siddharth@gmail.com>
2019-01-04 14:04:06 +0530

    scripts: used table driven tests for meta_test.go
    Converted meta_test.go of metamanager/dao to table driven tests.

--- pkg/metamanager/dao/meta_test.go
+++ pkg/metamanager/dao/meta_test.go
@@ -17,9 +17,11 @@ limitations under the License.
 package dao
 
 import (
+	"database/sql"
 	"errors"
 	"testing"
 
+	"github.com/astaxie/beego/orm"
 	"github.com/golang/mock/gomock"
 
 	"github.com/kubeedge/kubeedge/mocks/beego"
@@ -55,157 +57,358 @@ func initMocks(t *testing.T) {
 	dbm.DBAccess = ormerMock
 }
 
-//TestSaveMeta is function to initialize all global variable and test SaveMeta
+// TestSaveMeta is function to initialize all global variable and test SaveMeta
 func TestSaveMeta(t *testing.T) {
 	//Initialize Global Variables (Mocks)
 	initMocks(t)
-	//SaveMeta Success Case
-	ormerMock.EXPECT().Insert(gomock.Any()).Return(int64(1), nil).Times(1)
-	err := SaveMeta(&meta)
-	t.Run("SaveMetaSuccessCase", func(t *testing.T) {
-		if err != nil {
-			t.Errorf("Save Meta Case failed : wanted error nil and got error %v", err)
-		}
-	})
 
-	//SaveMeta Failure Case
-	ormerMock.EXPECT().Insert(gomock.Any()).Return(int64(1), failedDBOperationErr).Times(1)
-	err = SaveMeta(&meta)
-	t.Run("SaveMetaFailCase", func(t *testing.T) {
-		want := failedDBOperationErr
-		if want != err {
-			t.Errorf("Save Meta Case failed : wanted %v and got %v", want, err)
-		}
-	})
+	cases := []struct {
+		// name is name of the testcase
+		name string
+		// returnInt is first return of mock interface ormerMock
+		returnInt int64
+		// returnErr is second return of mock interface ormerMock which is also expected error
+		returnErr error
+	}{{
+		// Success Case
+		name:      "SuccessCase",
+		returnInt: int64(1),
+		returnErr: nil,
+	}, {
+		// Failure Case
+		name:      "FailureCase",
+		returnInt: int64(1),
+		returnErr: failedDBOperationErr,
+	},
+	}
+
+	// run the test cases
+	for _, test := range cases {
+		ormerMock.EXPECT().Insert(gomock.Any()).Return(test.returnInt, test.returnErr).Times(1)
+		err := SaveMeta(&meta)
+		t.Run(test.name, func(t *testing.T) {
+			if test.returnErr != err {
+				t.Errorf("Save Meta Case failed : wanted error %v and got error %v", test.returnErr, err)
+			}
+		})
+	}
 }
 
 // TestDeleteMetaByKey is function to test DeleteMetaByKey
 func TestDeleteMetaByKey(t *testing.T) {
-	querySeterMock.EXPECT().Filter(gomock.Any(), gomock.Any()).Return(querySeterMock).Times(1)
-	querySeterMock.EXPECT().Delete().Return(int64(1), failedDBOperationErr).Times(1)
-	ormerMock.EXPECT().QueryTable(gomock.Any()).Return(querySeterMock).Times(1)
-	err := DeleteMetaByKey("test")
-	t.Run("DeleteMetaByKeyFailCase", func(t *testing.T) {
-		want := failedDBOperationErr
-		if want != err {
-			t.Errorf("Delete Meta By Key Case failed : wanted %v and got %v", want, err)
-		}
-	})
+	cases := []struct {
+		// name is name of the testcase
+		name string
+		// filterReturn is the return of mock interface querySeterMock's filter function
+		filterReturn orm.QuerySeter
+		// deleteReturnInt is the first return of mock interface querySeterMock's delete function
+		deleteReturnInt int64
+		// deleteReturnErr is the second return of mock interface querySeterMocks's delete function also expected error
+		deleteReturnErr error
+		// queryTableReturn is the return of mock interface ormerMock's QueryTable function
+		queryTableReturn orm.QuerySeter
+	}{{
+		// Success Case
+		name:             "SuccessCase",
+		filterReturn:     querySeterMock,
+		deleteReturnInt:  int64(1),
+		deleteReturnErr:  nil,
+		queryTableReturn: querySeterMock,
+	}, {
+		// Failure Case
+		name:             "FailureCase",
+		filterReturn:     querySeterMock,
+		deleteReturnInt:  int64(0),
+		deleteReturnErr:  failedDBOperationErr,
+		queryTableReturn: querySeterMock,
+	},
+	}
+
+	// run the test cases
+	for _, test := range cases {
+		querySeterMock.EXPECT().Filter(gomock.Any(), gomock.Any()).Return(test.filterReturn).Times(1)
+		querySeterMock.EXPECT().Delete().Return(test.deleteReturnInt, test.deleteReturnErr).Times(1)
+		ormerMock.EXPECT().QueryTable(gomock.Any()).Return(test.queryTableReturn).Times(1)
+		err := DeleteMetaByKey("test")
+		t.Run(test.name, func(t *testing.T) {
+			if test.deleteReturnErr != err {
+				t.Errorf("Delete Meta By Key Case failed : wanted %v and got %v", test.deleteReturnErr, err)
+			}
+		})
+	}
 }
 
-//TestUpdateMeta is function to test UpdateMeta
+// TestUpdateMeta is function to test UpdateMeta
 func TestUpdateMeta(t *testing.T) {
-	ormerMock.EXPECT().Update(gomock.Any(), gomock.Any()).Return(int64(1), failedDBOperationErr).Times(1)
-	err := UpdateMeta(&meta)
-	t.Run("UpdateMetaFailCase", func(t *testing.T) {
-		want := failedDBOperationErr
-		if want != err {
-			t.Errorf("Update Meta Case failed : wanted %v and got %v", want, err)
-		}
-	})
+	cases := []struct {
+		// name is name of the testcase
+		name string
+		// returnInt is first return of mock interface ormerMock
+		returnInt int64
+		// returnErr is second return of mock interface ormerMock which is also expected error
+		returnErr error
+	}{{
+		// Success Case
+		name:      "SuccessCase",
+		returnInt: int64(1),
+		returnErr: nil,
+	}, {
+		// Failure Case
+		name:      "FailureCase",
+		returnInt: int64(0),
+		returnErr: failedDBOperationErr,
+	},
+	}
+
+	// run the test cases
+	for _, test := range cases {
+		ormerMock.EXPECT().Update(gomock.Any(), gomock.Any()).Return(test.returnInt, test.returnErr).Times(1)
+		err := UpdateMeta(&meta)
+		t.Run(test.name, func(t *testing.T) {
+			if test.returnErr != err {
+				t.Errorf("Update Meta Case failed : wanted %v and got %v", test.returnErr, err)
+			}
+		})
+	}
 }
 
 // TestInsertOrUpdate is function to test InsertOrUpdate
 func TestInsertOrUpdate(t *testing.T) {
-	rawSeterMock.EXPECT().Exec().Return(nil, failedDBOperationErr).Times(1)
-	ormerMock.EXPECT().Raw(gomock.Any(), gomock.Any()).Return(rawSeterMock).Times(1)
-	err := InsertOrUpdate(&meta)
-	t.Run("InsertOrUpdateFailCase", func(t *testing.T) {
-		want := failedDBOperationErr
-		if want != err {
-			t.Errorf("Insert or Update Meta Case failed : wanted %v and got %v", want, err)
-		}
-	})
+	cases := []struct {
+		// name is name of the testcase
+		name string
+		// returnSql is first return of mock interface rawSeterMock's Exec function
+		returnSql sql.Result
+		// returnErr is second return of mock interface rawSeterMock's Exec function which is also expected error
+		returnErr error
+		// returnRaw is the return of mock interface ormerMock's Raw function
+		returnRaw orm.RawSeter
+	}{{
+		// Success Case
+		name:      "SuccessCase",
+		returnSql: nil,
+		returnErr: nil,
+		returnRaw: rawSeterMock,
+	}, {
+		// Failure Case
+		name:      "FailureCase",
+		returnSql: nil,
+		returnErr: failedDBOperationErr,
+		returnRaw: rawSeterMock,
+	},
+	}
+
+	// run the test cases
+	for _, test := range cases {
+		rawSeterMock.EXPECT().Exec().Return(test.returnSql, test.returnErr).Times(1)
+		ormerMock.EXPECT().Raw(gomock.Any(), gomock.Any()).Return(test.returnRaw).Times(1)
+		err := InsertOrUpdate(&meta)
+		t.Run(test.name, func(t *testing.T) {
+			if test.returnErr != err {
+				t.Errorf("Insert or Update Meta Case failed : wanted %v and got %v", test.returnErr, err)
+			}
+		})
+	}
 }
 
 // TestUpdateMetaField is function to test UpdateMetaField
 func TestUpdateMetaField(t *testing.T) {
-	querySeterMock.EXPECT().Filter(gomock.Any(), gomock.Any()).Return(querySeterMock).Times(1)
-	querySeterMock.EXPECT().Update(gomock.Any()).Return(int64(1), failedDBOperationErr).Times(1)
-	ormerMock.EXPECT().QueryTable(gomock.Any()).Return(querySeterMock).Times(1)
-	err := UpdateMetaField("test", "test", "test")
-	t.Run("UpdateMetaFieldFailCase", func(t *testing.T) {
-		want := failedDBOperationErr
-		if want != err {
-			t.Errorf("Update Meta Field Case failed : wanted %v and got %v", want, err)
-		}
-	})
+	cases := []struct {
+		// name is name of the testcase
+		name string
+		// filterReturn is the return of mock interface querySeterMock's filter function
+		filterReturn orm.QuerySeter
+		// updateReturnInt is the first return of mock interface querySeterMock's update function
+		updateReturnInt int64
+		// updateReturnErr is the second return of mock interface querySeterMocks's update function also expected error
+		updateReturnErr error
+		// queryTableReturn is the return of mock interface ormerMock's QueryTable function
+		queryTableReturn orm.QuerySeter
+	}{{
+		// Success Case
+		name:             "SuccessCase",
+		filterReturn:     querySeterMock,
+		updateReturnInt:  int64(1),
+		updateReturnErr:  nil,
+		queryTableReturn: querySeterMock,
+	}, {
+		// Failure Case
+		name:             "FailureCase",
+		filterReturn:     querySeterMock,
+		updateReturnInt:  int64(0),
+		updateReturnErr:  failedDBOperationErr,
+		queryTableReturn: querySeterMock,
+	},
+	}
+
+	// run the test cases
+	for _, test := range cases {
+		querySeterMock.EXPECT().Filter(gomock.Any(), gomock.Any()).Return(test.filterReturn).Times(1)
+		querySeterMock.EXPECT().Update(gomock.Any()).Return(test.updateReturnInt, test.updateReturnErr).Times(1)
+		ormerMock.EXPECT().QueryTable(gomock.Any()).Return(test.queryTableReturn).Times(1)
+		err := UpdateMetaField("test", "test", "test")
+		t.Run(test.name, func(t *testing.T) {
+			if test.updateReturnErr != err {
+				t.Errorf("Update Meta Field Case failed : wanted %v and got %v", test.updateReturnErr, err)
+			}
+		})
+	}
 }
 
 // TestUpdateMetaFields is function to test UpdateMetaFields
 func TestUpdateMetaFields(t *testing.T) {
-	querySeterMock.EXPECT().Filter(gomock.Any(), gomock.Any()).Return(querySeterMock).Times(1)
-	querySeterMock.EXPECT().Update(gomock.Any()).Return(int64(1), failedDBOperationErr).Times(1)
-	ormerMock.EXPECT().QueryTable(gomock.Any()).Return(querySeterMock).Times(1)
-	err := UpdateMetaFields("test", nil)
-	t.Run("UpdateMetaFieldsFailCase", func(t *testing.T) {
-		want := failedDBOperationErr
-		if want != err {
-			t.Errorf("Update Meta Fields Case failed : wanted %v and got %v", want, err)
-		}
-	})
+	cases := []struct {
+		// name is name of the testcase
+		name string
+		// filterReturn is the return of mock interface querySeterMock's filter function
+		filterReturn orm.QuerySeter
+		// updateReturnInt is the first return of mock interface querySeterMock's update function
+		updateReturnInt int64
+		// updateReturnErr is the second return of mock interface querySeterMocks's update function also expected error
+		updateReturnErr error
+		// queryTableReturn is the return of mock interface ormerMock's QueryTable function
+		queryTableReturn orm.QuerySeter
+	}{{
+		// Success Case
+		name:             "SuccessCase",
+		filterReturn:     querySeterMock,
+		updateReturnInt:  int64(1),
+		updateReturnErr:  nil,
+		queryTableReturn: querySeterMock,
+	}, {
+		// Failure Case
+		name:             "FailureCase",
+		filterReturn:     querySeterMock,
+		updateReturnInt:  int64(0),
+		updateReturnErr:  failedDBOperationErr,
+		queryTableReturn: querySeterMock,
+	},
+	}
+
+	// run the test cases
+	for _, test := range cases {
+		querySeterMock.EXPECT().Filter(gomock.Any(), gomock.Any()).Return(test.filterReturn).Times(1)
+		querySeterMock.EXPECT().Update(gomock.Any()).Return(test.updateReturnInt, test.updateReturnErr).Times(1)
+		ormerMock.EXPECT().QueryTable(gomock.Any()).Return(test.queryTableReturn).Times(1)
+		err := UpdateMetaFields("test", nil)
+		t.Run(test.name, func(t *testing.T) {
+			if test.updateReturnErr != err {
+				t.Errorf("Update Meta Fields Case failed : wanted %v and got %v", test.updateReturnErr, err)
+			}
+		})
+	}
 }
 
 // TestQueryMeta is function to test QueryMeta
 func TestQueryMeta(t *testing.T) {
-	//failure case
-	querySeterMock.EXPECT().All(gomock.Any()).Return(int64(1), failedDBOperationErr).Times(1)
-	querySeterMock.EXPECT().Filter(gomock.Any(), gomock.Any()).Return(querySeterMock).Times(1)
-	ormerMock.EXPECT().QueryTable(gomock.Any()).Return(querySeterMock).Times(1)
-	meta, err := QueryMeta("test", "test")
-	t.Run("QueryMetaCheckError", func(t *testing.T) {
-		want := failedDBOperationErr
-		if want != err {
-			t.Errorf("Query Meta Case failed : wanted %v and got %v", want, err)
-		}
-	})
+	cases := []struct {
+		// name is name of the testcase
+		name string
+		// filterReturn is the return of mock interface querySeterMock's filter function
+		filterReturn orm.QuerySeter
+		// allReturnInt is the first return of mock interface querySeterMock's all function
+		allReturnInt int64
+		// allReturnErr is the second return of mock interface querySeterMocks's all function also expected error
+		allReturnErr error
+		// queryTableReturn is the return of mock interface ormerMock's QueryTable function
+		queryTableReturn orm.QuerySeter
+	}{{
+		// Success Case
+		name:             "SuccessCase",
+		filterReturn:     querySeterMock,
+		allReturnInt:     int64(1),
+		allReturnErr:     nil,
+		queryTableReturn: querySeterMock,
+	}, {
+		// Failure Case
+		name:             "FailureCase",
+		filterReturn:     querySeterMock,
+		allReturnInt:     int64(0),
+		allReturnErr:     failedDBOperationErr,
+		queryTableReturn: querySeterMock,
+	},
+	}
 
-	//success case
+	// fakeDao is used to set the argument of All function
 	fakeDao := new([]Meta)
 	fakeDaoArray := make([]Meta, 1)
 	fakeDaoArray[0] = Meta{Key: "Test"}
 	fakeDao = &fakeDaoArray
-	querySeterMock.EXPECT().All(gomock.Any()).SetArg(0, *fakeDao).Return(int64(1), nil).Times(1)
-	querySeterMock.EXPECT().Filter(gomock.Any(), gomock.Any()).Return(querySeterMock).Times(1)
-	ormerMock.EXPECT().QueryTable(gomock.Any()).Return(querySeterMock).Times(1)
-	meta, err = QueryMeta("test", "test")
-	t.Run("QueryMetaSuccessCase", func(t *testing.T) {
-		if err != nil {
-			t.Errorf("Query Meta Case Failed, wanted error nil and got error %v", err)
-		}
-		want := 1
-		if want != len(*meta) {
-			t.Errorf("Query Meta Case failed wanted length %v and got length %v", want, len(*meta))
-		}
-	})
+
+	// run the test cases
+	for _, test := range cases {
+		querySeterMock.EXPECT().All(gomock.Any()).SetArg(0, *fakeDao).Return(test.allReturnInt, test.allReturnErr).Times(1)
+		querySeterMock.EXPECT().Filter(gomock.Any(), gomock.Any()).Return(test.filterReturn).Times(1)
+		ormerMock.EXPECT().QueryTable(gomock.Any()).Return(test.queryTableReturn).Times(1)
+		meta, err := QueryMeta("test", "test")
+		t.Run(test.name, func(t *testing.T) {
+			if test.allReturnErr != err {
+				t.Errorf("Query Meta Case Failed : wanted error %v and got error %v", test.allReturnErr, err)
+				return
+			}
+
+			if err == nil {
+				if len(*meta) != 1 {
+					t.Errorf("Query Meta Case failed: wanted length 1 and got length %v", len(*meta))
+				}
+			}
+		})
+	}
 }
 
 // TestQueryAllMeta is function to test QueryAllMeta
 func TestQueryAllMeta(t *testing.T) {
-	//failure case
-	querySeterMock.EXPECT().All(gomock.Any()).Return(int64(1), failedDBOperationErr).Times(1)
-	querySeterMock.EXPECT().Filter(gomock.Any(), gomock.Any()).Return(querySeterMock).Times(1)
-	ormerMock.EXPECT().QueryTable(gomock.Any()).Return(querySeterMock).Times(1)
-	meta, err := QueryAllMeta("test", "test")
-	t.Run("QueryAllMetaCheckError", func(t *testing.T) {
-		want := failedDBOperationErr
-		if want != err {
-			t.Errorf("Query All Meta Case failed : wanted %v and got %v", want, err)
-		}
-	})
+	cases := []struct {
+		// name is name of the testcase
+		name string
+		// filterReturn is the return of mock interface querySeterMock's filter function
+		filterReturn orm.QuerySeter
+		// allReturnInt is the first return of mock interface querySeterMock's all function
+		allReturnInt int64
+		// allReturnErr is the second return of mock interface querySeterMocks's all function also expected error
+		allReturnErr error
+		// queryTableReturn is the return of mock interface ormerMock's QueryTable function
+		queryTableReturn orm.QuerySeter
+	}{{
+		// Success Case
+		name:             "SuccessCase",
+		filterReturn:     querySeterMock,
+		allReturnInt:     int64(1),
+		allReturnErr:     nil,
+		queryTableReturn: querySeterMock,
+	}, {
+		// Failure Case
+		name:             "FailureCase",
+		filterReturn:     querySeterMock,
+		allReturnInt:     int64(0),
+		allReturnErr:     failedDBOperationErr,
+		queryTableReturn: querySeterMock,
+	},
+	}
 
-	//success case
-	querySeterMock.EXPECT().All(gomock.Any()).Return(int64(1), nil).Times(1)
-	querySeterMock.EXPECT().Filter(gomock.Any(), gomock.Any()).Return(querySeterMock).Times(1)
-	ormerMock.EXPECT().QueryTable(gomock.Any()).Return(querySeterMock).Times(1)
-	meta, err = QueryAllMeta("test", "test")
-	t.Run("QueryAllMetaSuccessCase", func(t *testing.T) {
-		if err != nil {
-			t.Errorf("Query Meta All Case Failed, wanted error nil and got error %v", err)
-		}
-		want := 0
-		if want != len(*meta) {
-			t.Errorf("Query All Meta Case failed wanted length %v and got length %v", want, len(*meta))
-		}
-	})
+	// fakeDao is used to set the argument of All function
+	fakeDao := new([]Meta)
+	fakeDaoArray := make([]Meta, 1)
+	fakeDaoArray[0] = Meta{Key: "Test", Value: "Test"}
+	fakeDao = &fakeDaoArray
+
+	// run the test cases
+	for _, test := range cases {
+		querySeterMock.EXPECT().All(gomock.Any()).SetArg(0, *fakeDao).Return(test.allReturnInt, test.allReturnErr).Times(1)
+		querySeterMock.EXPECT().Filter(gomock.Any(), gomock.Any()).Return(test.filterReturn).Times(1)
+		ormerMock.EXPECT().QueryTable(gomock.Any()).Return(test.queryTableReturn).Times(1)
+		meta, err := QueryAllMeta("test", "test")
+		t.Run(test.name, func(t *testing.T) {
+			if test.allReturnErr != err {
+				t.Errorf("Query All Meta Case Failed : wanted error %v and got error %v", test.allReturnErr, err)
+				return
+			}
+
+			if err == nil {
+				if len(*meta) != 1 {
+					t.Errorf("Query All Meta Case failed: wanted length 1 and got length %v", len(*meta))
+				}
+			}
+		})
+	}
 }
