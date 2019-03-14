055a2c2fdb1c30347ac9a436c098a61fc978dce4
Pavan Kumar D <pavan.kumar.d1@huawei.com>
2019-03-14 19:54:21 +0530

    E2E Automation framework changes

---
+++ tests/e2e/deployment/deployment_suite_test.go
@@ -0,0 +1,99 @@
+/*
+Copyright 2019 The KubeEdge Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package deployment
+
+import (
+	"net/http"
+	"os/exec"
+	"testing"
+	"time"
+
+	"github.com/kubeedge/kubeedge/tests/e2e/utils"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+//context to load config and access across the package
+var (
+	ctx *utils.TestContext
+	cfg utils.Config
+)
+
+const (
+	runController = "cd ${GOPATH}/src/github.com/kubeedge/kubeedge/cloud/edgecontroller; nohup ./edgecontroller > edgecontroller.log 2>&1 &"
+	runEdgecore   = "cd ${GOPATH}/src/github.com/kubeedge/kubeedge/edge/; nohup ./edge_core > edge_core.log 2>&1 &"
+)
+
+//Function to run the Ginkgo Test
+func TestEdgecoreAppDeployment(t *testing.T) {
+	var nodeName string
+	RegisterFailHandler(Fail)
+	var _ = BeforeSuite(func() {
+		utils.InfoV6("Before Suite Execution")
+		cfg = utils.LoadConfig()
+		ctx = utils.NewTestContext(cfg)
+		nodeName = "integration-node-" + utils.GetRandomString(10)
+		//Generate Cerificates for Edge and Cloud nodes copy to respective folders
+		cmd := exec.Command("bash", "-x", "scripts/generate_cert.sh")
+		err := utils.PrintCombinedOutput(cmd)
+		Expect(err).Should(BeNil())
+		//Do the neccessary config changes in Cloud and Edge nodes
+		cmd = exec.Command("bash", "-x", "scripts/setup.sh", nodeName, ctx.Cfg.ApiServer)
+		err = utils.PrintCombinedOutput(cmd)
+		Expect(err).Should(BeNil())
+		time.Sleep(1 * time.Second)
+		//Run ./edgecontroller binary
+		cmd = exec.Command("sh", "-c", runController)
+		err = utils.PrintCombinedOutput(cmd)
+		time.Sleep(5 * time.Second)
+		Expect(err).Should(BeNil())
+		//Register the Edge Node to Master
+		err = utils.RegisterNodeToMaster(ctx, nodeName, NodeHandler)
+		Expect(err).Should(BeNil())
+		//Run ./edge_core after node registration
+		cmd = exec.Command("sh", "-c", runEdgecore)
+		err = utils.PrintCombinedOutput(cmd)
+		time.Sleep(5 * time.Second)
+		//Check node successfully registered or not
+		Eventually(func() string {
+			status := utils.CheckNodeReadyStatus(ctx, NodeHandler, nodeName)
+			utils.Info("Node Name: %v, Node Status: %v", nodeName, status)
+			return status
+		}, "60s", "4s").Should(Equal("Running"), "Node register to the k8s master is unsuccessfull !!")
+
+	})
+	AfterSuite(func() {
+		By("After Suite Execution....!")
+		//Deregister the edge node from master
+		err := utils.DeRegisterNodeFromMaster(ctx, NodeHandler, nodeName)
+		Expect(err).Should(BeNil())
+		Eventually(func() int {
+			statuscode := utils.CheckNodeDeleteStatus(ctx, NodeHandler, nodeName)
+			utils.Info("Node Name: %v, Node Statuscode: %v", nodeName, statuscode)
+			return statuscode
+		}, "60s", "4s").Should(Equal(http.StatusNotFound), "Node register to the k8s master is unsuccessfull !!")
+		//Run the Cleanup steps to kill edge_core and edgecontroller binaries
+		cmd := exec.Command("bash", "-x", "scripts/cleanup.sh")
+		err = utils.PrintCombinedOutput(cmd)
+		Expect(err).Should(BeNil())
+		time.Sleep(2 * time.Second)
+		utils.Info("Cleanup is Successfull !!")
+	})
+
+	RunSpecs(t, "kubeedge App Deploymet Suite")
+}
---
+++ tests/e2e/utils/node.go
@@ -0,0 +1,126 @@
+/*
+Copyright 2019 The KubeEdge Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package utils
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	. "github.com/onsi/gomega"
+	"k8s.io/api/core/v1"
+)
+
+//Function to deregister the node from master
+func DeRegisterNodeFromMaster(ctx *TestContext, nodehandler, nodename string) error {
+	err, resp := SendHttpRequest(http.MethodDelete, ctx.Cfg.ApiServer+nodehandler+"/"+nodename)
+	if err != nil {
+		Failf("Sending SenHttpRequest failed: %v", err)
+		return err
+	}
+	defer resp.Body.Close()
+	Expect(resp.StatusCode).Should(Equal(http.StatusOK))
+
+	return nil
+}
+
+//function to generate the node request body
+func GenerateNodeReqBody(nodeid string) (error, map[string]interface{}) {
+	var temp map[string]interface{}
+
+	body := fmt.Sprintf(`{"kind": "Node","apiVersion": "v1","metadata": {"name": "%s","labels": {"name": "edgenode"}}}`, nodeid)
+	err := json.Unmarshal([]byte(body), &temp)
+	if err != nil {
+		Failf("Unmarshal body failed: %v", err)
+		return err, temp
+	}
+
+	return nil, temp
+}
+
+//Function to register node to master
+func RegisterNodeToMaster(ctx *TestContext, UID, nodehandler string) error {
+	err, body := GenerateNodeReqBody(UID)
+	if err != nil {
+		Failf("Unmarshal body failed: %v", err)
+		return err
+	}
+
+	client := &http.Client{}
+	t := time.Now()
+	nodebody, err := json.Marshal(body)
+	if err != nil {
+		Failf("Marshal body failed: %v", err)
+		return err
+	}
+	BodyBuf := bytes.NewReader(nodebody)
+	req, err := http.NewRequest(http.MethodPost, ctx.Cfg.ApiServer+nodehandler, BodyBuf)
+	req.Header.Set("Content-Type", "application/json")
+	if err != nil {
+		Failf("Frame HTTP request failed: %v", err)
+		return err
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		Failf("Sending HTTP request failed: %v", err)
+		return err
+	}
+	InfoV6("%s %s %v in %v", req.Method, req.URL, resp.Status, time.Now().Sub(t))
+	defer resp.Body.Close()
+
+	Expect(resp.StatusCode).Should(Equal(http.StatusCreated))
+	return nil
+}
+
+//function to get node status
+func CheckNodeReadyStatus(ctx *TestContext, nodehandler, nodename string) string {
+	var node v1.Node
+	var nodeStatus = "unknown"
+	err, resp := SendHttpRequest(http.MethodGet, ctx.Cfg.ApiServer+nodehandler+"/"+nodename)
+	if err != nil {
+		Failf("Sending SenHttpRequest failed: %v", err)
+		return nodeStatus
+	}
+	defer resp.Body.Close()
+
+	contents, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		Failf("HTTP Response reading has failed: %v", err)
+		return nodeStatus
+	}
+	err = json.Unmarshal(contents, &node)
+	if err != nil {
+		Failf("Unmarshal HTTP Response has failed: %v", err)
+		return nodeStatus
+	}
+
+	return string(node.Status.Phase)
+}
+
+//function to node delete status
+func CheckNodeDeleteStatus(ctx *TestContext, nodehandler, nodename string) int {
+	err, resp := SendHttpRequest(http.MethodGet, ctx.Cfg.ApiServer+nodehandler+"/"+nodename)
+	if err != nil {
+		Failf("Sending SenHttpRequest failed: %v", err)
+		return -1
+	}
+	defer resp.Body.Close()
+	return resp.StatusCode
+}
