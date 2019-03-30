62148e08447f5b7cf7fb6aaec7394d94dc0ec62a
lidiyag <lidiya.nixon@huawei.com>
2019-03-30 20:04:41 +0530

    This commit adds unit test for package lane.

---
+++ pkg/lane/lane_test.go
@@ -0,0 +1,59 @@
+package lane
+
+import (
+	"github.com/golang/mock/gomock"
+	"github.com/kubeedge/viaduct/mocks"
+	"github.com/kubeedge/viaduct/pkg/api"
+	"github.com/gorilla/websocket"
+	"reflect"
+	"testing"
+)
+
+// mockStream is mock of interface Stream.
+var mockStream *mocks.MockStream
+
+// initMocks is function to initialize mocks.
+func initMocks(t *testing.T){
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	mockStream =mocks.NewMockStream(mockCtrl)
+}
+
+// TestNewLane is function to test NewLane().
+func TestNewLane(t *testing.T) {
+	initMocks(t)
+	wsConn:=&websocket.Conn{}
+	tests := []struct {
+		name string
+		protoType string
+		van       interface{}
+		want Lane
+	}{
+
+		{
+			name:"TestQuic",
+			protoType:api.ProtocolTypeQuic,
+			van:mockStream,
+			want:&QuicLane{},
+		},
+		{
+			name:"TestWS",
+			protoType:api.ProtocolTypeWS,
+			van:wsConn,
+			want:&WSLane{},
+		},
+		{
+			name:"TestDefault",
+			protoType:"default",
+			van:"",
+			want:nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewLane(tt.protoType, tt.van); reflect.TypeOf(got)!=reflect.TypeOf(tt.want) {
+				t.Errorf("NewLane() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
