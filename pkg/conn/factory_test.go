4f4ae747d15c517b5660b0d71385ff7eb6e18f51
lidiyag <lidiya.nixon@huawei.com>
2019-03-30 20:12:32 +0530

    This commit adds unit tests for viaduct/pkg/conn/factory.go

---
+++ pkg/conn/factory_test.go
@@ -0,0 +1,58 @@
+package conn
+
+import (
+	"github.com/golang/mock/gomock"
+	"github.com/gorilla/websocket"
+	"github.com/kubeedge/viaduct/mocks"
+	"github.com/kubeedge/viaduct/pkg/api"
+	"reflect"
+	"testing"
+)
+
+// mockSession is mock implementation of interface Session.
+var mockSession *mocks.MockSession
+
+// muxhandler is mock implementation of Mux Handler.
+var muxhandler *mocks.MockHandler
+
+// initMocks is function to initialize mocks.
+func initMocks(t *testing.T){
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	mockSession =mocks.NewMockSession(mockCtrl)
+	muxhandler=mocks.NewMockHandler(mockCtrl)
+}
+
+// TestNewConnection is function to test NewConnection().
+func TestNewConnection(t *testing.T) {
+	initMocks(t)
+	wsConn:=&websocket.Conn{}
+	tests := []struct {
+		name string
+		opts *ConnectionOptions
+		want Connection
+	}{
+		{
+			name:"TestQuic",
+			opts:&ConnectionOptions{ConnType:api.ProtocolTypeQuic,Base:mockSession,Handler:muxhandler},
+			want:&QuicConnection{},
+		},
+		{
+			name:"TestWS",
+			opts:&ConnectionOptions{ConnType:api.ProtocolTypeWS,Base:wsConn,Handler:muxhandler},
+			want:&WSConnection{},
+		},
+		{
+			name:"TestDefault",
+			opts:&ConnectionOptions{ConnType:"default"},
+			want:nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewConnection(tt.opts); reflect.TypeOf(got)!=reflect.TypeOf(tt.want) {
+				t.Errorf("NewConnection() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
