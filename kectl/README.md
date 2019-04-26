dd790909c0b66a0e819c14005a58affd1db0ec34
samy2019 <sameer.sarkar@huawei.com>
2019-04-27 00:30:44 +0530

    updated vendor required for kectl

---
+++ kectl/README.md
@@ -0,0 +1,17 @@
+# KubeEdge Installer
+
+[Kubeedge Installer Doc](<https:https://github.com/samy2019/kubeedge/blob/bc14b1ee9f71b023b1e9a6faf68a0ee01ff35450/docs/proposals/kubeedgeadm-scope.md/>)
+
+#### Build KubeEdgeinstaller
+
+ ```shell
+    cd $GOPATH/src/github.com/kubeedge/kubeedge/kectl
+    
+     go build kectl.go
+     
+     mv kectl /usr/local/bin
+ ```
+  
+#### Execute command to get started
+
+kectl --help 
---
+++ kectl/app/cmd/cloud.go
@@ -0,0 +1,55 @@
+/*
+Copyright 2019 The Kubeedge Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cmd
+
+import (
+	"io"
+
+	"github.com/spf13/cobra"
+
+	cloud "github.com/kubeedge/kubeedge/kectl/app/cmd/cloud"
+)
+
+var (
+	cloudLongDescription = `
+cloud commands help in operating with KubeEdge's cloud component.
+`
+	cloudExample = `
+kectl cloud init <arguments> 
+kectl cloud reset 
+`
+)
+
+// NewCmdCloud represents the cloud commands
+func NewCmdCloud(out io.Writer) *cobra.Command {
+
+	var cmd = &cobra.Command{
+		Use:     "cloud",
+		Short:   "Cloud component command option for KubeEdge",
+		Long:    cloudLongDescription,
+		Args:    cobra.MinimumNArgs(1),
+		Example: cloudExample,
+	}
+
+	cmd.AddCommand(cloud.NewCloudInit(out, nil))
+	cmd.AddCommand(cloud.NewCloudReset(out))
+	return cmd
+}
+
+func init() {
+
+}
