fae03b7beae102916413a99d45cbdf512e232d44
Kevin Wang <wangzefeng@huawei.com>
2019-05-14 11:36:42 +0800

    fix review comments on membership criteria

--- docs/getting-started/community-membership.sh
+++ docs/getting-started/community-membership.sh
@@ -1,4 +1,4 @@
-:# KubeEdge Community membership
+:# KubeEdge community membership
 
 **Note :** This document keeps changing based on the status and feedback of KubeEdge Community.
 
@@ -7,8 +7,8 @@ This document gives a brief overview of the KubeEdge community roles with the re
 | Role | Requirements | Responsibilities | Privileges |
 | -----| ---------------- | ------------ | -------|
 | [Member](#member) | Sponsor from 2 approvers, active in community, contributed to KubeEdge | Welcome and guide new contributors | KubeEdge GitHub organization Member |
-| [Approver](#approver) | Sponsor from 2 maintainers, has good experience and knowledge of domain, actively contributed to code and review  | Review and approve contributions from community members | Write access to relevant repository |
-| [Maintainer](#maintainer) | Sponsor from 2 owners, shown good technical judgement in feature design/development and PR review | Participate in release planning and feature development/maintenance | Write access to relevant repository. Name entry in Maintainers file of the repository |
+| [Approver](#approver) | Sponsor from 2 maintainers, has good experience and knowledge of domain, actively contributed to code and review  | Review and approve contributions from community members | Write access to specific packagies in relevant repository |
+| [Maintainer](#maintainer) | Sponsor from 2 owners, shown good technical judgement in feature design/development and PR review | Participate in release planning and feature development/maintenance | Top level write access to relevant repository. Name entry in Maintainers file of the repository |
 | [Owner](#owner) | Sponsor from 3 owners, helps drive the overall KubeEdge project | Drive the overall technical roadmap of the project and set priorities of activities in release planning | KubeEdge GitHub organization Admin access |
 
 
@@ -19,10 +19,6 @@ This document gives a brief overview of the KubeEdge community roles with the re
 Members are active participants in the community who contribute by authoring PRs,
 reviewing issues/PRs or participate in community discussions on slack/mailing list. 
 
-### Privileges
-
-- Member of the Kubernetes GitHub organization
-- Can be assigned to issues and PRs and community members can also request their review
 
 ### Requirements
 
@@ -33,12 +29,16 @@ reviewing issues/PRs or participate in community discussions on slack/mailing li
     - Participating in community discussions on slack/mailing list
     - Participate in KubeEdge community meetings
 
+
 ### Responsibilities 
 
+- Member of the KubeEdge GitHub organization
+- Can be assigned to issues and PRs and community members can also request their review
 - Participate in assigned issues and PRs
 - Welcome new contributors
 - Guide new contributors to relevant docs/files
 - Help/Motivate new members in contributing to KubeEdge
+
 
 ## Approver
 
@@ -59,13 +59,13 @@ They have actively participated in the issue/PR reviews and have identified rele
 - Review code to maintain/improve code quality
 - Acknowledge and work on review requests from community members
 - May approve code contributions for acceptance related to relevant expertise
+- Have 'write access' to specific packages inside a repo, enforced via bot
 - Continue to contribute and guide other community members to contribute in KubeEdge project
 
 ## Maintainer
 
 Maintainers are approvers who have shown good technical judgement in feature design/development in the past.
 Has overall knowledge of the project and features in the project.
-
 
 ### Requirements
 
@@ -82,21 +82,14 @@ Has overall knowledge of the project and features in the project.
 - Analyze and propose new features/enhancements in KubeEdge project
 - Demonstrate sound technical judgement
 - Mentor contributors and approvers
-- Have write access to relevant repository (able to Merge PRs) 
+- Have top level write access to relevant repository (able click Merge PR button when manual check-in is necessary)
 - Name entry in Maintainers file of the repository
 - Participate & Drive design/development of multiple features
 
 ## Owner
 
 Owners are maintainers who have helped drive the overall project direction.
-Has deep understanding of KubeEdge and related domain and has major say in release planning
-
-### Privileges
-
-- Have admin access to all repositories.
-- May get admin access to relevant repository based on requirement
-- Participate & Drive design/development of multiple features
-- Merge PRs 
+Has deep understanding of KubeEdge and related domain and facilitates major agreement in release planning
 
 ### Requirements
 
@@ -106,7 +99,7 @@ Has deep understanding of KubeEdge and related domain and has major say in relea
 - Not opposed by any project owner
 - Helped in driving the overall project
 
-### Responsibilities
+### Responsibilities and Privileges
 
 - Make technical decisions for the overall project
 - Drive the overall technical roadmap of the project
@@ -114,6 +107,9 @@ Has deep understanding of KubeEdge and related domain and has major say in relea
 - Guide and mentor all other community members
 - Ensure all community members are following Code of Conduct
 - Although given admin access to all repositories, make sure all PRs are properly reviewed and merged
+- May get admin access to relevant repository based on requirement
+- Participate & Drive design/development of multiple features
+
 
 **Note :** These roles are applicable only for KubeEdge github organization and repositories. Currently KubeEdge doesn't have a formal process for review and acceptance into these roles. We will come-up with a process soon.
 
