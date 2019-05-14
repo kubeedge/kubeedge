a8f38925308ef38c8c2f9780c2dde2ba227b178d
Cindy Xing <li.xing1@huawei.com>
2019-05-14 23:29:52 +0000

    Fix upon Kevin's code review comments

--- edgesite/pkg/controller/constants/service.go
+++ edgesite/pkg/controller/constants/service.go
@@ -22,11 +22,11 @@ const (
 
 	// Nvidia Constants
 	// NvidiaGPUStatusAnnotationKey is the key of the node annotation for GPU status
-	NvidiaGPUStatusAnnotationKey = "huawei.com/gpu-status"
+	NvidiaGPUStatusAnnotationKey = "gpu.kubeedge.io/gpu-status"
 	// NvidiaGPUDecisionAnnotationKey is the key of the pod annotation for scheduler GPU decision
-	NvidiaGPUDecisionAnnotationKey = "huawei.com/gpu-decision"
+	NvidiaGPUDecisionAnnotationKey = "gpu.kubeedge.io/gpu-decision"
 	// NvidiaGPUScalarResourceName is the device plugin resource name used for special handling
-	NvidiaGPUScalarResourceName = "nvidia.com/gpu"
+	NvidiaGPUScalarResourceName = "gpu.kubeedge.io/gpu"
 	// NvidiaGPUMaxUsage is the maximum possible usage of a GPU in millis
 	NvidiaGPUMaxUsage = 1000
 )
--- edgesite/pkg/controller/controller/upstream.go
+++ edgesite/pkg/controller/controller/upstream.go
@@ -284,6 +284,7 @@ func (uc *UpstreamController) updateNodeStatus(stop chan struct{}) {
 					continue
 				}
 
+				// TODO: comment below for test failure. Needs to decide whether to keep post troubleshoot
 				// In case the status stored at metadata service is outdated, update the heartbeat automatically
 				/*for i := range nodeStatusRequest.Status.Conditions {
 					if time.Now().Sub(nodeStatusRequest.Status.Conditions[i].LastHeartbeatTime.Time) > config.KubeUpdateNodeFrequency {
