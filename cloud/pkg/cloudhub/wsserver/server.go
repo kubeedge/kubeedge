b21b7c237055db1aa26cb493c4f3ae02b10b7019
zhangjie <iamkadisi@163.com>
2019-05-20 14:16:10 +0800

    set default websocket
    Signed-off-by: zhangjie <iamkadisi@163.com>

--- cloud/pkg/cloudhub/wsserver/server.go
+++ cloud/pkg/cloudhub/wsserver/server.go
@@ -1,12 +1,9 @@
 package wsserver
 
 import (
-	"crypto/tls"
-	"crypto/x509"
 	"encoding/json"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"os"
 	"strings"
@@ -16,14 +13,12 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
 
-	"github.com/kubeedge/kubeedge/cloud/pkg/cloudhub/channelq"
-	hubio "github.com/kubeedge/kubeedge/cloud/pkg/cloudhub/common/io"
-	emodel "github.com/kubeedge/kubeedge/cloud/pkg/cloudhub/common/model"
-	"github.com/kubeedge/kubeedge/cloud/pkg/cloudhub/common/util"
-
 	bhLog "github.com/kubeedge/beehive/pkg/common/log"
 	"github.com/kubeedge/beehive/pkg/core/context"
 	"github.com/kubeedge/beehive/pkg/core/model"
+	"github.com/kubeedge/kubeedge/cloud/pkg/cloudhub/channelq"
+	hubio "github.com/kubeedge/kubeedge/cloud/pkg/cloudhub/common/io"
+	emodel "github.com/kubeedge/kubeedge/cloud/pkg/cloudhub/common/model"
 )
 
 // ExitCode exit code
@@ -367,51 +362,4 @@ func (f *FilterWriter) Write(p []byte) (n int, err error) {
 		return 0, nil
 	}
 	return os.Stderr.Write(p)
-}
-
-// StartCloudHub starts the cloud hub service
-func StartCloudHub(config *util.Config, eventq *channelq.ChannelEventQueue) error {
-	// init certificate
-	pool := x509.NewCertPool()
-	ok := pool.AppendCertsFromPEM(config.Ca)
-	if !ok {
-		return fmt.Errorf("fail to load ca content")
-	}
-	cert, err := tls.X509KeyPair(config.Cert, config.Key)
-	if err != nil {
-		return err
-	}
-	tlsConfig := tls.Config{
-		ClientCAs:    pool,
-		ClientAuth:   tls.RequestClientCert,
-		Certificates: []tls.Certificate{cert},
-		MinVersion:   tls.VersionTLS12,
-		CipherSuites: []uint16{tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256},
-	}
-
-	// init handler
-	ah := &AccessHandle{
-		EventHandle: &EventHandle{
-			KeepaliveInterval: config.KeepaliveInterval,
-			WriteTimeout:      config.WriteTimeout,
-			EventQueue:        eventq,
-		},
-		NodeLimit: config.NodeLimit,
-	}
-	EventHandler = ah.EventHandle
-
-	router := mux.NewRouter()
-	router.HandleFunc(PathEvent, ah.ServeEvent)
-
-	// start server
-	s := http.Server{
-		Addr:      fmt.Sprintf("%s:%d", config.Address, config.Port),
-		Handler:   router,
-		TLSConfig: &tlsConfig,
-		ErrorLog:  log.New(&FilterWriter{}, "", log.LstdFlags),
-	}
-	bhLog.LOGGER.Infof("Start cloud hub service")
-	go s.ListenAndServeTLS("", "")
-
-	return nil
 }
