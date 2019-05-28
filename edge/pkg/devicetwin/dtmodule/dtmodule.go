8e464efe5771cbc2ff6d23210b8a3b697032aa3d
Jan Unterbrink <jan.unterbrink@subpath.de>
2019-05-28 09:22:38 +0200

    using switch case instead of if - else if

--- edge/pkg/devicetwin/dtmodule/dtmodule.go
+++ edge/pkg/devicetwin/dtmodule/dtmodule.go
@@ -1,8 +1,6 @@
 package dtmodule
 
 import (
-	"strings"
-
 	"github.com/kubeedge/beehive/pkg/common/log"
 	"github.com/kubeedge/kubeedge/edge/pkg/devicetwin/dtcontext"
 	"github.com/kubeedge/kubeedge/edge/pkg/devicetwin/dtmanager"
@@ -17,28 +15,30 @@ type DTModule struct {
 // InitWorker init worker
 func (dm *DTModule) InitWorker(recv chan interface{}, confirm chan interface{}, heartBeat chan interface{}, dtContext *dtcontext.DTContext) {
 
-	if strings.Compare(dm.Name, "MemModule") == 0 {
+	switch dm.Name {
+	case "MemModule":
 		dm.Worker = dtmanager.MemWorker{
 			Group: "MemModule",
 			Worker: dtmanager.Worker{ReceiverChan: recv,
 				ConfirmChan:   confirm,
 				HeartBeatChan: heartBeat,
 				DTContexts:    dtContext}}
-	} else if strings.Compare(dm.Name, "TwinModule") == 0 {
+	case "TwinModule":
 		dm.Worker = dtmanager.TwinWorker{
 			Group: "TwinModule",
 			Worker: dtmanager.Worker{ReceiverChan: recv,
 				ConfirmChan:   confirm,
 				HeartBeatChan: heartBeat,
 				DTContexts:    dtContext}}
-	} else if strings.Compare(dm.Name, "DeviceModule") == 0 {
+
+	case "DeviceModule":
 		dm.Worker = dtmanager.DeviceWorker{
 			Group: "DeviceModule",
 			Worker: dtmanager.Worker{ReceiverChan: recv,
 				ConfirmChan:   confirm,
 				HeartBeatChan: heartBeat,
 				DTContexts:    dtContext}}
-	} else if strings.Compare(dm.Name, "CommModule") == 0 {
+	case "CommModule":
 		dm.Worker = dtmanager.CommWorker{
 			Group: "CommModule",
 			Worker: dtmanager.Worker{ReceiverChan: recv,
