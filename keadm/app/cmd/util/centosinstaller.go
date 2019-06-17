74edd4cd54400af792c41aff55eeaa5446144b44
root <root@instance-5.asia-east1-b.c.molten-shield-222606.internal>
2019-06-17 06:28:39 +0000

    everything ok

--- keadm/app/cmd/util/centosinstaller.go
+++ keadm/app/cmd/util/centosinstaller.go
@@ -502,11 +502,14 @@ func (c *CentOS) InstallKubeEdge() error {
 		fmt.Println("Expected or Default KubeEdge version", c.KubeEdgeVersion, "is already downloaded")
 		goto SKIPDOWNLOADAND
 	}
+	//dwnldURL = fmt.Sprintf("cd %s && curl -O  -k  --progress-bar %s/v%s/%s", KubeEdgePath, KubeEdgeDownloadURL, c.KubeEdgeVersion, filename)
+	cmd = &Command{Cmd: exec.Command("sh", "-c", "yum install -y wget")}
+	cmd.ExecuteCommand()
 
 	for i := 0; i < downloadRetryTimes; i++ {
 		//Download the tar from repo
-		//dwnldURL = fmt.Sprintf("cd %s && wget -k --no-check-certificate --progress=bar:force %s/v%s/%s", KubeEdgePath, KubeEdgeDownloadURL, c.KubeEdgeVersion, filename)
-		dwnldURL = fmt.Sprintf("cd %s && curl -O  -k  --progress-bar %s/v%s/%s", KubeEdgePath, KubeEdgeDownloadURL, c.KubeEdgeVersion, filename)
+		dwnldURL = fmt.Sprintf("cd %s && wget -k --no-check-certificate --progress=bar:force %s/v%s/%s", KubeEdgePath, KubeEdgeDownloadURL, c.KubeEdgeVersion, filename)
+		//dwnldURL = fmt.Sprintf("cd %s && curl -O  -k  --progress-bar %s/v%s/%s", KubeEdgePath, KubeEdgeDownloadURL, c.KubeEdgeVersion, filename)
 		cmd = &Command{Cmd: exec.Command("sh", "-c", dwnldURL)}
 		if err := cmd.ExecuteCmdShowOutput(); err != nil {
 			return err
@@ -525,8 +528,8 @@ func (c *CentOS) InstallKubeEdge() error {
 		fmt.Printf("%s \n\n", cmd.GetStdOutput())
 
 		fmt.Printf("%s content: \n", checksumFilename)
-		//cmdStr = fmt.Sprintf("wget -qO- %s/v%s/%s", KubeEdgeDownloadURL, c.KubeEdgeVersion, checksumFilename)
-		cmdStr = fmt.Sprintf("curl -O %s/v%s/%s", KubeEdgeDownloadURL, c.KubeEdgeVersion, checksumFilename)
+		cmdStr = fmt.Sprintf("wget -qO- %s/v%s/%s", KubeEdgeDownloadURL, c.KubeEdgeVersion, checksumFilename)
+		//cmdStr = fmt.Sprintf("curl -O %s/v%s/%s", KubeEdgeDownloadURL, c.KubeEdgeVersion, checksumFilename)
 		cmd = &Command{Cmd: exec.Command("sh", "-c", cmdStr)}
 		cmd.ExecuteCommand()
 		actualChecksum := cmd.GetStdOutput()
