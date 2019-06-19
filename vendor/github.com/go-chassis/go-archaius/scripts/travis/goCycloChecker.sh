8d6966ad3a567e91849eb4c6249363b639126d11
anyushun <anyushun@huawei.com>
2019-06-19 17:39:36 +0530

    update dependencies version

--- vendor/github.com/go-chassis/go-archaius/scripts/travis/goCycloChecker.sh
+++
@@ -1,8 +0,0 @@
-diff -u <(echo -n) <(find . -name "*.go" -not -path ".git/*" | grep -v _test | xargs gocyclo -over 16)
-if [ $? == 0 ]; then
-	echo "All function has less cyclomatic complexity..."
-	exit 0
-else
-	echo "Fucntions/function has more cyclomatic complexity..."
-	exit 1
-fi
--- vendor/github.com/go-chassis/go-archaius/scripts/travis/goLintChecker.sh
+++
@@ -1,8 +0,0 @@
-diff -u <(echo -n) <(golint ./... | grep -v stutters | grep -v _test | grep -v examples)
-if [ $? == 0 ]; then
-	echo "No GoLint warnings found"
-	exit 0
-else
-	echo "GoLint Warnings found"
-	exit 1
-fi
