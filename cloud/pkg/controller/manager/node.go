14be439b12eda6eee623d880f853685cbcab80d4
Jan Unterbrink <jan.unterbrink@subpath.de>
2019-06-24 08:13:57 +0200

    using std. way to query nodes

--- cloud/pkg/controller/manager/node.go
+++ cloud/pkg/controller/manager/node.go
@@ -1,12 +1,9 @@
 package manager
 
 import (
-	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/cache"
 )
 
 const (
@@ -16,28 +13,23 @@ const (
 
 // NodesManager manage all events of nodes by SharedInformer
 type NodesManager struct {
-	events chan watch.Event
+	events <-chan watch.Event
 }
 
 // Events return the channel save events from watch nodes change
-func (nm *NodesManager) Events() chan watch.Event {
+func (nm *NodesManager) Events() <-chan watch.Event {
 	return nm.events
 }
 
 // NewNodesManager create NodesManager by kube clientset and namespace
 func NewNodesManager(kubeClient *kubernetes.Clientset, namespace string) (*NodesManager, error) {
-	set := labels.Set{NodeRoleKey: NodeRoleValue}
-	selector := labels.SelectorFromSet(set)
-	optionModifier := func(options *metav1.ListOptions) {
-		options.LabelSelector = selector.String()
+	nodes := kubeClient.CoreV1().Nodes()
+	watchInterface, err := nodes.Watch(metav1.ListOptions{})
+	if err != nil {
+		return nil, err
 	}
-	lw := cache.NewFilteredListWatchFromClient(kubeClient.CoreV1().RESTClient(), "nodes", namespace, optionModifier)
-	events := make(chan watch.Event)
-	rh := NewCommonResourceEventHandler(events)
-	si := cache.NewSharedInformer(lw, &v1.Node{}, 0)
-	si.AddEventHandler(rh)
-	stopNever := make(chan struct{})
-	go si.Run(stopNever)
 
-	return &NodesManager{events: events}, nil
+	events := watchInterface.ResultChan()
+	nm := &NodesManager{events: events}
+	return nm, nil
 }
