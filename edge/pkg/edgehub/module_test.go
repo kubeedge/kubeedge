748c3234b5cfbaef9d9f5cccdcc8f9faae88f1b5
kadisi <iamkadisi@163.com>
2019-06-29 22:02:13 +0800

    add apache license 2.0
    Signed-off-by: kadisi <iamkadisi@163.com>

--- edge/pkg/edgehub/module_test.go
+++ edge/pkg/edgehub/module_test.go
@@ -1,12 +1,28 @@
+/*
+Copyright 2019 The KubeEdge Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package edgehub
 
 import (
+	"testing"
+	"time"
+
 	"github.com/kubeedge/beehive/pkg/core"
 	"github.com/kubeedge/beehive/pkg/core/context"
 	"github.com/kubeedge/beehive/pkg/core/model"
 	commodule "github.com/kubeedge/kubeedge/edge/pkg/common/modules"
-	"testing"
-	"time"
 )
 
 // coreContext is beehive context used for communication between modules
--- edgemesh/pkg/server/tcp_test.go
+++ edgemesh/pkg/server/tcp_test.go
@@ -1,10 +1,24 @@
+/*
+Copyright 2019 The KubeEdge Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package server_test
 
 import (
 	"bufio"
 	"bytes"
 	"fmt"
-	"github.com/kubeedge/beehive/pkg/common/log"
 	"io/ioutil"
 	"net"
 	"net/http"
@@ -12,6 +26,8 @@ import (
 	"strings"
 	"testing"
 	"time"
+
+	"github.com/kubeedge/beehive/pkg/common/log"
 
 	"github.com/go-chassis/go-chassis/core/handler"
 	"github.com/go-chassis/go-chassis/core/invocation"
