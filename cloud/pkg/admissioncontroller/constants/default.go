09c2ac2fac34bf27f4d07a3d5c7fd575388221e9
Dave Chen <dave.chen@arm.com>
2019-08-13 16:46:16 +0800

    Make it more flexible to config admit functions
    http handler is changed to `DefaultServeMux`, different admit functions
    could be register for different resources.
    
    Signed-off-by: Dave Chen <dave.chen@arm.com>

--- cloud/pkg/admissioncontroller/constants/default.go
+++ cloud/pkg/admissioncontroller/constants/default.go
@@ -1,9 +1,9 @@
 package constants
 
 const (
-	ExternalAdmissionWebhookName = "validation-devices.kubeedge.io"
-	Port                         = 8899
-	NamespaceName                = "default"
-	ServiceName                  = "webhook-service"
-	SecretName                   = "webhook-secret"
+	WebhookNameDeviceModel = "deny-unwanted-devicemodel.k8s.io"
+	Port                   = 8899
+	NamespaceName          = "default"
+	ServiceName            = "webhook-service"
+	SecretName             = "webhook-secret"
 )
