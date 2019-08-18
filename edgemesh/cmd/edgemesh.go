ed973f0b1e39821979dd79f464d2184f77456ff1
anyscoding <anyushun@huawei.com>
2019-08-18 08:21:59 +0800

    cleanup edgemesh
    1. everything init in pkg/server/server.go, so cmd/edgemesh.go no need
    do it
    2. go-chassis upgrade to v1.6.1, no need to init HystrixConfig now

--- edgemesh/cmd/edgemesh.go
+++ edgemesh/cmd/edgemesh.go
@@ -2,18 +2,11 @@ package main
 
 import (
 	"flag"
-
-	"github.com/go-chassis/go-chassis/control"
-	"github.com/go-chassis/go-chassis/core/config"
-	"github.com/go-chassis/go-chassis/core/loadbalancer"
-	"github.com/go-chassis/go-chassis/core/registry"
 	"github.com/kubeedge/kubeedge/edgemesh/pkg"
 	"github.com/spf13/pflag"
 	"k8s.io/klog"
 
 	_ "github.com/kubeedge/kubeedge/edgemesh/pkg/panel"
-	edgeregistry "github.com/kubeedge/kubeedge/edgemesh/pkg/registry"
-	"github.com/kubeedge/kubeedge/edgemesh/pkg/resolver"
 	"github.com/kubeedge/kubeedge/edgemesh/pkg/server"
 )
 
@@ -22,28 +15,8 @@ func main() {
 	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
 	pflag.Parse()
 
-	//Initialize the resolvers
-	r := &resolver.MyResolver{"http"}
-	resolver.RegisterResolver(r)
-
 	pkg.Register()
 
-	//Initialize the handlers
-	//
-	config.Init()
-	config.GlobalDefinition.Panel.Infra = "fake"
-	opts := control.Options{
-		Infra:   config.GlobalDefinition.Panel.Infra,
-		Address: config.GlobalDefinition.Panel.Settings["address"],
-	}
-	config.GlobalDefinition.Ssl = make(map[string]string)
-
-	control.Init(opts)
-	opt := registry.Options{}
-	registry.DefaultServiceDiscoveryService = edgeregistry.NewServiceDiscovery(opt)
-	loadbalancer.InstallStrategy(loadbalancer.StrategyRandom, func() loadbalancer.Strategy {
-		return &loadbalancer.RandomStrategy{}
-	})
 	//Start server
 	server.StartTCP()
 }
