0ef7e9921065602e07058907411026e343aa1be9
edisonxiang <xiang.edison@gmail.com>
2019-08-19 15:38:24 +0800

    add metaclient func for csi

--- edge/pkg/metamanager/client/node.go
+++ edge/pkg/metamanager/client/node.go
@@ -1,9 +1,16 @@
 package client
 
 import (
+	"encoding/json"
+	"fmt"
+
 	api "k8s.io/api/core/v1"
 
 	"github.com/kubeedge/beehive/pkg/core/context"
+	"github.com/kubeedge/beehive/pkg/core/model"
+	"github.com/kubeedge/kubeedge/edge/pkg/common/message"
+	"github.com/kubeedge/kubeedge/edge/pkg/common/modules"
+	"github.com/kubeedge/kubeedge/edge/pkg/metamanager"
 )
 
 //NodesGetter to get node interface
@@ -38,6 +45,12 @@ func (c *nodes) Create(cm *api.Node) (*api.Node, error) {
 }
 
 func (c *nodes) Update(cm *api.Node) error {
+	resource := fmt.Sprintf("%s/%s/%s", c.namespace, model.ResourceTypeNode, cm.Name)
+	nodeMsg := message.BuildMsg(modules.MetaGroup, "", modules.EdgedModuleName, resource, model.UpdateOperation, cm)
+	_, err := c.send.SendSync(nodeMsg)
+	if err != nil {
+		return fmt.Errorf("update node failed, err: %v", err)
+	}
 	return nil
 }
 
@@ -46,5 +59,54 @@ func (c *nodes) Delete(name string) error {
 }
 
 func (c *nodes) Get(name string) (*api.Node, error) {
-	return nil, nil
+	resource := fmt.Sprintf("%s/%s/%s", c.namespace, model.ResourceTypeNode, name)
+	nodeMsg := message.BuildMsg(modules.MetaGroup, "", modules.EdgedModuleName, resource, model.QueryOperation, nil)
+	msg, err := c.send.SendSync(nodeMsg)
+	if err != nil {
+		return nil, fmt.Errorf("get node failed, err: %v", err)
+	}
+
+	var content []byte
+	switch msg.Content.(type) {
+	case []byte:
+		content = msg.GetContent().([]byte)
+	default:
+		content, err = json.Marshal(msg.GetContent())
+		if err != nil {
+			return nil, fmt.Errorf("marshal message to node failed, err: %v", err)
+		}
+	}
+
+	if msg.GetOperation() == model.ResponseOperation && msg.GetSource() == metamanager.MetaManagerModuleName {
+		return handleNodeFromMetaDB(content)
+	}
+	return handleNodeFromMetaManager(content)
+}
+
+func handleNodeFromMetaDB(content []byte) (*api.Node, error) {
+	var lists []string
+	err := json.Unmarshal(content, &lists)
+	if err != nil {
+		return nil, fmt.Errorf("unmarshal message to node list from db failed, err: %v", err)
+	}
+
+	if len(lists) != 1 {
+		return nil, fmt.Errorf("node length from meta db is %d", len(lists))
+	}
+
+	var node *api.Node
+	err = json.Unmarshal([]byte(lists[0]), &node)
+	if err != nil {
+		return nil, fmt.Errorf("unmarshal message to node from db failed, err: %v", err)
+	}
+	return node, nil
+}
+
+func handleNodeFromMetaManager(content []byte) (*api.Node, error) {
+	var node *api.Node
+	err := json.Unmarshal(content, &node)
+	if err != nil {
+		return nil, fmt.Errorf("unmarshal message to node failed, err: %v", err)
+	}
+	return node, nil
 }
