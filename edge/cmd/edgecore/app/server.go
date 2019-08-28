ba0404b89fa3fb2b043d3cb26cdbf4d524344dfd
zhangjie <iamkadisi@163.com>
2019-08-28 14:04:56 +0800

    make  flag --version install of command version
    Signed-off-by: zhangjie <iamkadisi@163.com>

--- edge/cmd/edgecore/app/server.go
+++ edge/cmd/edgecore/app/server.go
@@ -1,9 +1,16 @@
 package app
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
+	"k8s.io/apiserver/pkg/util/term"
+	cliflag "k8s.io/component-base/cli/flag"
+	"k8s.io/component-base/cli/globalflag"
+	"k8s.io/klog"
 
 	"github.com/kubeedge/beehive/pkg/core"
+	"github.com/kubeedge/kubeedge/edge/cmd/edgecore/app/options"
 	"github.com/kubeedge/kubeedge/edge/pkg/common/dbm"
 	"github.com/kubeedge/kubeedge/edge/pkg/devicetwin"
 	"github.com/kubeedge/kubeedge/edge/pkg/edged"
@@ -13,10 +20,14 @@ import (
 	"github.com/kubeedge/kubeedge/edge/pkg/servicebus"
 	"github.com/kubeedge/kubeedge/edge/test"
 	edgemesh "github.com/kubeedge/kubeedge/edgemesh/pkg"
+	"github.com/kubeedge/kubeedge/pkg/util/flag"
+	"github.com/kubeedge/kubeedge/pkg/version"
+	"github.com/kubeedge/kubeedge/pkg/version/verflag"
 )
 
 // NewEdgeCoreCommand create edgecore cmd
 func NewEdgeCoreCommand() *cobra.Command {
+	opts := options.NewEdgeCoreOptions()
 	cmd := &cobra.Command{
 		Use: "edgecore",
 		Long: `Edgecore is the core edge part of KubeEdge, which contains six modules: devicetwin, edged, 
@@ -31,13 +42,36 @@ is the message processor between edged and edgehub. It is also responsible for s
 to/from a lightweight database (SQLite).ServiceBus is a HTTP client to interact with HTTP servers (REST), 
 offering HTTP client capabilities to components of cloud to reach HTTP servers running at edge. `,
 		Run: func(cmd *cobra.Command, args []string) {
+			verflag.PrintAndExitIfRequested()
+			flag.PrintFlags(cmd.Flags())
+
+			// To help debugging, immediately log version
+			klog.Infof("Version: %+v", version.Get())
+
 			registerModules()
 			// start all modules
 			core.Run()
 		},
 	}
+	fs := cmd.Flags()
+	namedFs := opts.Flags()
+	verflag.AddFlags(namedFs.FlagSet("global"))
+	globalflag.AddGlobalFlags(namedFs.FlagSet("global"), cmd.Name())
+	for _, f := range namedFs.FlagSets {
+		fs.AddFlagSet(f)
+	}
 
-	cmd.AddCommand(NewCmdVersion())
+	usageFmt := "Usage:\n  %s\n"
+	cols, _, _ := term.TerminalSize(cmd.OutOrStdout())
+	cmd.SetUsageFunc(func(cmd *cobra.Command) error {
+		fmt.Fprintf(cmd.OutOrStderr(), usageFmt, cmd.UseLine())
+		cliflag.PrintSections(cmd.OutOrStderr(), namedFs, cols)
+		return nil
+	})
+	cmd.SetHelpFunc(func(cmd *cobra.Command, args []string) {
+		fmt.Fprintf(cmd.OutOrStdout(), "%s\n\n"+usageFmt, cmd.Long, cmd.UseLine())
+		cliflag.PrintSections(cmd.OutOrStdout(), namedFs, cols)
+	})
 
 	return cmd
 }
