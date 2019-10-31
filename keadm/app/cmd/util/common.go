3c63d85cc5dbcace2a0c1cfc28eb915f21d7502d
Philippe Coval <rzr@users.sf.net>
2019-10-31 16:11:46 +0100

    keadm: Add constants for exectuable path
    Remove duplicated paths.
    
    This variable could be eventually patched downstream
    to "usr/bin" (for deeper integration in distros).
    
    Forwarded: https://github.com/kubeedge/kubeedge/pull/1261
    Relate-to: https://github.com/kubeedge/kubeedge/issues/1246
    Change-Id: Ie93c1880bb258515a8c0a756510e505425d5246c
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- keadm/app/cmd/util/common.go
+++ keadm/app/cmd/util/common.go
@@ -43,6 +43,7 @@ const (
 
 	KubeEdgeDownloadURL       = "https://github.com/kubeedge/kubeedge/releases/download"
 	KubeEdgePath              = "/etc/kubeedge/"
+	KubeEdgeUsrBinPath        = "/usr/local/bin"
 	KubeEdgeConfPath          = KubeEdgePath + "kubeedge/edge/conf"
 	KubeEdgeBinaryName        = "edgecore"
 	KubeEdgeDefaultCertPath   = KubeEdgePath + "certs/"
