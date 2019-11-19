8fd24d4a84e8275ba0833a1023018824d549e5c5
zhangjie <iamkadisi@163.com>
2019-11-19 18:55:41 +0800

    Optimize the initialization of the devicetwin module global variable
    Signed-off-by: zhangjie <iamkadisi@163.com>

---
+++ edge/pkg/devicetwin/config/config.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"sync"
+
+	"github.com/kubeedge/kubeedge/staging/src/github.com/kubeedge/beehive/pkg/common/config"
+
+	"k8s.io/klog"
+)
+
+var c Configure
+var once sync.Once
+
+type Configure struct {
+	NodeID string
+}
+
+func InitConfigure() {
+	once.Do(func() {
+		var errs []error
+		defer func() {
+			if len(errs) != 0 {
+				for _, e := range errs {
+					klog.Errorf("%v", e)
+				}
+				klog.Error("init devicetwin config error")
+				os.Exit(1)
+			} else {
+				klog.Infof("init devicetwin config successfully，config info %++v", c)
+			}
+		}()
+		nodeID, err := config.CONFIG.GetValue("edgehub.controller.node-id").ToString()
+		if err != nil {
+			errs = append(errs, fmt.Errorf("get edgehub.controller.node-id key error %v"), err)
+		}
+		c = Configure{
+			NodeID: nodeID,
+		}
+	})
+}
+
+func Get() Configure {
+	return c
+}
