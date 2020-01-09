5d160fa66f7bc9b87254079dc6df777db24d4ce7
fisherxu <xufei40@huawei.com>
2020-01-09 11:59:50 +0800

    add reliable sync api

--- cloud/pkg/apis/reliablesyncs/v1alpha1/register.go
+++ cloud/pkg/apis/reliablesyncs/v1alpha1/register.go
@@ -1,27 +1,17 @@
-/*
-Copyright 2019 The KubeEdge Authors.
+// Copyright 2020 The KubeEdge Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-// NOTE: Boilerplate only.  Ignore this file.
-
-// Package v1alpha1 contains API Schema definitions for the devices v1alpha1 API group
-// +k8s:openapi-gen=true
-// +k8s:deepcopy-gen=package,register
-// +k8s:conversion-gen=kubeedge/cloud/pkg/apis/devices
-// +k8s:defaulter-gen=TypeMeta
-// +groupName=devices.kubeedge.io
 package v1alpha1
 
 import (
@@ -45,7 +35,7 @@ const (
 
 var (
 	// SchemeGroupVersion is the group version used to register these objects.
-	SchemeGroupVersion = schema.GroupVersion{Group: "devices.kubeedge.io", Version: "v1alpha1"}
+	SchemeGroupVersion = schema.GroupVersion{Group: GroupName, Version: Version}
 )
 
 func init() {
@@ -63,10 +53,10 @@ func Resource(resource string) schema.GroupResource {
 // addKnownTypes adds the set of types defined in this package to the supplied scheme.
 func addKnownTypes(scheme *runtime.Scheme) error {
 	scheme.AddKnownTypes(SchemeGroupVersion,
-		&Device{},
-		&DeviceList{},
-		&DeviceModel{},
-		&DeviceModelList{},
+		&ObjectSync{},
+		&ObjectSyncList{},
+		&ClusterObjectSync{},
+		&ClusterObjectSyncList{},
 	)
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil
