222a2f07a2621c43caee8bfe7aceb2f6d65acf88
zhangjie <iamkadisi@163.com>
2020-01-13 13:46:21 +0800

    add InitDB function
    Signed-off-by: zhangjie <iamkadisi@163.com>

--- edge/pkg/common/dbm/config.go
+++ edge/pkg/common/dbm/config.go
@@ -1,4 +1,4 @@
-package config
+package dbm
 
 import (
 	"os"
--- edge/pkg/common/dbm/db.go
+++ edge/pkg/common/dbm/db.go
@@ -1,37 +1,46 @@
 package dbm
 
 import (
+	"sync"
+
 	"github.com/astaxie/beego/orm"
-	//Blank import to run only the init function
-	_ "github.com/mattn/go-sqlite3"
 	"k8s.io/klog"
 
-	commonconfig "github.com/kubeedge/kubeedge/edge/pkg/common/config"
+	//Blank import to run only the init function
+	_ "github.com/mattn/go-sqlite3"
 )
 
 //DBAccess is Ormer object interface for all transaction processing and switching database
 var DBAccess orm.Ormer
+var onceDB sync.Once
 
 //InitDBManager initialises the database by syncing the database schema and creating orm
 func InitDBManager() {
-	commonconfig.InitConfigure()
+	// TODO will be deleted after component api featur @kadisi
+	InitConfigure()
+	InitDB(Get().DriverName, Get().DBName, Get().DataSource)
+}
 
-	// TODO need add switch case @kadisi
-	if err := orm.RegisterDriver(commonconfig.Get().DriverName, orm.DRSqlite); err != nil {
-		klog.Fatalf("Failed to register driver: %v", err)
-	}
+// TODO Ref to component api config @kadisi
+func InitDB(driverName, dbName, dataSource string) {
+	onceDB.Do(func() {
+		// TODO need add switch case @kadisi
+		if err := orm.RegisterDriver(driverName, orm.DRSqlite); err != nil {
+			klog.Fatalf("Failed to register driver: %v", err)
+		}
 
-	if err := orm.RegisterDataBase(
-		commonconfig.Get().DBName,
-		commonconfig.Get().DriverName,
-		commonconfig.Get().DataSource); err != nil {
-		klog.Fatalf("Failed to register db: %v", err)
-	}
+		if err := orm.RegisterDataBase(
+			dbName,
+			driverName,
+			dataSource); err != nil {
+			klog.Fatalf("Failed to register db: %v", err)
+		}
 
-	// sync database schema
-	orm.RunSyncdb(commonconfig.Get().DBName, false, true)
+		// sync database schema
+		orm.RunSyncdb(dbName, false, true)
 
-	// create orm
-	DBAccess = orm.NewOrm()
-	DBAccess.Using(commonconfig.Get().DBName)
+		// create orm
+		DBAccess = orm.NewOrm()
+		DBAccess.Using(dbName)
+	})
 }
