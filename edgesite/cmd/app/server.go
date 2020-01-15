421d32872d78226ac78fb4f49101a2e8b80fe8dc
zhangjie <iamkadisi@163.com>
2020-01-15 10:29:56 +0800

    edgesite: edged, edgecontroller, metamanager use new config api
    Signed-off-by: zhangjie <iamkadisi@163.com>

--- edgesite/cmd/app/server.go
+++ edgesite/cmd/app/server.go
@@ -13,7 +13,6 @@ import (
 
 	"github.com/kubeedge/beehive/pkg/core"
 	"github.com/kubeedge/kubeedge/cloud/pkg/edgecontroller"
-	"github.com/kubeedge/kubeedge/edge/pkg/common/dbm"
 	"github.com/kubeedge/kubeedge/edge/pkg/edged"
 	"github.com/kubeedge/kubeedge/edge/pkg/metamanager"
 	"github.com/kubeedge/kubeedge/edgesite/cmd/app/options"
@@ -88,8 +87,7 @@ runs on edge nodes and manages containerized applications.`,
 
 // registerModules register all the modules started in edgesite
 func registerModules(c *v1alpha1.EdgeSiteConfig) {
-	edged.Register()
-	edgecontroller.Register()
-	metamanager.Register()
-	dbm.InitDBManager()
+	edged.Register(c.Modules.Edged)
+	edgecontroller.Register(c.Modules.EdgeController, c.KubeAPIConfig, c.Modules.Edged.HostnameOverride, true)
+	metamanager.Register(c.Modules.MetaManager, c.DataBase)
 }
