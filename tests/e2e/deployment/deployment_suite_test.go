0876a068588e08bab5cfdf386db68978d0612f3b
fisherxu <xufei40@huawei.com>
2020-02-24 11:09:31 +0800

    add device e2e test

--- tests/e2e/deployment/deployment_suite_test.go
+++ tests/e2e/deployment/deployment_suite_test.go
@@ -60,7 +60,8 @@ func TestEdgecoreAppDeployment(t *testing.T) {
 			utils.Infof("Node Name: %v, Node Status: %v", nodeName, status)
 			return status
 		}, "60s", "4s").Should(Equal("Running"), "Node register to the k8s master is unsuccessfull !!")
-
+		err := utils.MqttConnect()
+		Expect(err).To(BeNil())
 	})
 	AfterSuite(func() {
 		By("After Suite Execution....!")
