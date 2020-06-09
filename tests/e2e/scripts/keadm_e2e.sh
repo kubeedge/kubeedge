e1e0fd348aff510a83795bbba501254edbeffc9a
Xiang Dai <long0dai@foxmail.com>
2020-06-09 15:24:47 +0800

    test/e2e: Optimize keadm e2e test script
    Signed-off-by: Xiang Dai <long0dai@foxmail.com>

--- tests/e2e/scripts/keadm_e2e.sh
+++ tests/e2e/scripts/keadm_e2e.sh
@@ -14,41 +14,52 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-curpath=$PWD
+KUBEEDGE_ROOT=$PWD
+WORKDIR=$(dirname $0)
+E2E_DIR=$(realpath $(dirname $0)/..)
 
-make all WHAT=keadm
+function cleanup() {
+  ps aux | grep '[e]dgecore' | awk '{print $2}' | xargs -r sudo kill -9
+  ps aux | grep '[c]loudcore' | awk '{print $2}' | xargs -r sudo kill -9
+  kind delete cluster --name test
+}
 
-cd `dirname $0`
-workdir=`pwd`
-cd $workdir
+function build_keadm() {
+  make all WHAT=keadm
+  cd $E2E_DIR
+  ginkgo build -r keadm/
+}
 
-cd ../
-echo $PWD
+function prepare_cluster() {
+  kind create cluster --name test
 
-ginkgo build -r keadm/
+  echo "wait the control-plane ready..."
+  kubectl wait --for=condition=Ready node/test-control-plane --timeout=60s
 
-kind create cluster --name test
-kubectl create clusterrolebinding system:anonymous --clusterrole=cluster-admin --user=system:anonymous
+  kubectl create clusterrolebinding system:anonymous --clusterrole=cluster-admin --user=system:anonymous
 
-# edge side don't support kind cni now, delete kind cni plugin for workaround
-kubectl delete daemonset kindnet -nkube-system
+  # edge side don't support kind cni now, delete kind cni plugin for workaround
+  kubectl delete daemonset kindnet -nkube-system
+}
 
-export KUBECONFIG=$HOME/.kube/config
-sudo mkdir -p /var/lib/kubeedge
-sudo ${curpath}/_output/local/bin/keadm init --kube-config=$KUBECONFIG --advertise-address=127.0.0.1
+function start_kubeedge() {
+  sudo mkdir -p /var/lib/kubeedge
+  cd $KUBEEDGE_ROOT
+  sudo -E _output/local/bin/keadm init --kube-config=$KUBECONFIG --advertise-address=127.0.0.1
+  export MASTER_IP=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' test-control-plane`
 
-export MASTER_IP=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' test-control-plane`
+  # ensure tokensecret is generated
+  while true; do
+      sleep 3
+      kubectl get secret -nkubeedge 2>/dev/null | grep -q tokensecret && break
+  done
 
-# TODO: Check the secret has been created
-sleep 3
+  export TOKEN=$(sudo _output/local/bin/keadm gettoken --kube-config=$KUBECONFIG)
+  sudo -E CHECK_EDGECORE_ENVIRONMENT="false" _output/local/bin/keadm join --token=$TOKEN --cloudcore-ipport=127.0.0.1:10000 --edgenode-name=edge-node
 
-TOKEN=`sudo ${curpath}/_output/local/bin/keadm gettoken --kube-config=$KUBECONFIG`
-echo $TOKEN
-sudo CHECK_EDGECORE_ENVIRONMENT="false" ${curpath}/_output/local/bin/keadm join --token=$TOKEN --cloudcore-ipport=127.0.0.1:10000 --edgenode-name=edge-node
-
-#Pre-configurations required for running the suite.
-#Any new config addition required corresponding code changes.
-cat >config.json<<END
+  #Pre-configurations required for running the suite.
+  #Any new config addition required corresponding code changes.
+  cat > $E2E_DIR/config.json <<END
 {
         "image_url": ["nginx", "nginx"],
         "k8smasterforkubeedge":"https://$MASTER_IP:6443",
@@ -59,27 +70,47 @@ cat >config.json<<END
 }
 END
 
-# TODO: check edgenode running
-sleep 10s
+  # ensure edgenode is ready
+  while true; do
+      sleep 3
+      kubectl get node | grep edge-node | grep -q Ready && break
+  done
+}
 
-:> /tmp/testcase.log
-./keadm/keadm.test $debugflag 2>&1 | tee -a /tmp/testcase.log
+function run_test() {
+  :> /tmp/testcase.log
+  cd $E2E_DIR
+  ./keadm/keadm.test $debugflag 2>&1 | tee -a /tmp/testcase.log
 
-#stop the edgecore after the test completion
-grep  -e "Running Suite" -e "SUCCESS\!" -e "FAIL\!" /tmp/testcase.log | sed -r 's/\x1B\[([0-9];)?([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g' | sed -r 's/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g'
-echo "Integration Test Final Summary Report"
-echo "==============================================="
-echo "Total Number of Test cases = `grep "Ran " /tmp/testcase.log | awk '{sum+=$2} END {print sum}'`"
-passed=`grep -e "SUCCESS\!" -e "FAIL\!" /tmp/testcase.log | awk '{print $3}' | sed -r "s/\x1B\[([0-9];)?([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g" | awk '{sum+=$1} END {print sum}'`
-echo "Number of Test cases PASSED = $passed"
-fail=`grep -e "SUCCESS\!" -e "FAIL\!" /tmp/testcase.log | awk '{print $6}' | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g" | awk '{sum+=$1} END {print sum}'`
-echo "Number of Test cases FAILED = $fail"
-echo "==================Result Summary======================="
+  #stop the edgecore after the test completion
+  grep  -e "Running Suite" -e "SUCCESS\!" -e "FAIL\!" /tmp/testcase.log | sed -r 's/\x1B\[([0-9];)?([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g' | sed -r 's/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g'
+  echo "Integration Test Final Summary Report"
+  echo "======================================================="
+  echo "Total Number of Test cases = `grep "Ran " /tmp/testcase.log | awk '{sum+=$2} END {print sum}'`"
+  passed=`grep -e "SUCCESS\!" -e "FAIL\!" /tmp/testcase.log | awk '{print $3}' | sed -r "s/\x1B\[([0-9];)?([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g" | awk '{sum+=$1} END {print sum}'`
+  echo "Number of Test cases PASSED = $passed"
+  fail=`grep -e "SUCCESS\!" -e "FAIL\!" /tmp/testcase.log | awk '{print $6}' | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g" | awk '{sum+=$1} END {print sum}'`
+  echo "Number of Test cases FAILED = $fail"
+  echo "==================Result Summary======================="
 
-if [ "$fail" != "0" ];then
-    echo "Integration suite has failures, Please check !!"
-    exit 1
-else
-    echo "Integration suite successfully passed all the tests !!"
-    exit 0
-fi
+  if [ "$fail" != "0" ];then
+      echo "Integration suite has failures, Please check !!"
+      exit 1
+  else
+      echo "Integration suite successfully passed all the tests !!"
+      exit 0
+  fi
+}
+
+set -Ee
+trap cleanup EXIT
+trap cleanup ERR
+
+build_keadm
+
+export KUBECONFIG=$HOME/.kube/config
+prepare_cluster
+
+start_kubeedge
+
+run_test
