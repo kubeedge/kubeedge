0ef0734a7533e5bdfd831460f0a52729d225369f
shenkonghui <18767151789@163.com>
2020-07-21 19:01:21 +0800

    add keadm debug entry

---
+++ keadm/cmd/keadm/app/cmd/debug/debug.go
@@ -0,0 +1,53 @@
+/*
+Copyright 2020 The KubeEdge Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package debug
+
+import (
+	"errors"
+	"github.com/spf13/cobra"
+	"io"
+)
+
+var (
+	edgeDebugLongDescription = `
+"keadm help command provide debug function to help diagnose the cluster
+`
+)
+
+// NewEdgeDebug returns KubeEdge edge debug command.
+func NewEdgeDebug(out io.Writer) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "debug",
+		Short:   "provide debug function to help diagnose the cluster",
+		Long:    edgeDebugLongDescription,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if len(args) < 1 {
+				return errors.New("requires a subcommand")
+			}
+			return Execute()
+		},
+	}
+	// add subCommand collect
+	//cmd.AddCommand(collect.NewEdgeCollect(out,nil))
+
+	return cmd
+}
+
+//Execute the debug command
+func Execute() error {
+	return nil
+}
