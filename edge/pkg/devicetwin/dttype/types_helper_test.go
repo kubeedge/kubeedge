68cc143c510cf9324989ef56c4b601fe86d9de65
Congrool <783928876@qq.com>
2020-10-03 18:02:06 +0800

    Revert "Test: remove useless codes"
    This reverts commit c3743cc048393ca664f242f6b9005257d981250b.

--- edge/pkg/devicetwin/dttype/types_helper_test.go
+++ edge/pkg/devicetwin/dttype/types_helper_test.go
@@ -590,8 +590,8 @@ func createDevice() []*Device {
 // createMembershipGetResult is function to create MembershipGetResult with given base message.
 func createMembershipGetResult(message BaseMessage) MembershipGetResult {
 	attrs := createMessageAttribute()
-	var devices []Device
-	device := Device{
+	devices := []*Device{}
+	device := &Device{
 		ID:          "id1",
 		Name:        "SensorTag",
 		Description: "Sensor",
@@ -600,8 +600,17 @@ func createMembershipGetResult(message BaseMessage) MembershipGetResult {
 		Attributes:  attrs,
 	}
 	devices = append(devices, device)
-
-	memGetResult := MembershipGetResult{BaseMessage: message, Devices: devices}
+	wantDevice := []Device{}
+	wantDev := Device{
+		ID:          device.ID,
+		Name:        device.Name,
+		Description: device.Description,
+		State:       device.State,
+		LastOnline:  device.LastOnline,
+		Attributes:  device.Attributes,
+	}
+	wantDevice = append(wantDevice, wantDev)
+	memGetResult := MembershipGetResult{BaseMessage: message, Devices: wantDevice}
 	return memGetResult
 }
 
