68df79db726a8f4a459b7ee0ac51f8764a60bcba
Yin Ding <dingyin@gmail.com>
2020-10-08 22:16:36 -0700

    fix lint issues

--- cloud/cmd/csidriver/app/server.go
+++ cloud/cmd/csidriver/app/server.go
@@ -17,63 +17,63 @@ limitations under the License.
 package app
 
 import (
-	"fmt"
+    "fmt"
 
-	"github.com/spf13/cobra"
-	"k8s.io/component-base/term"
-	cliflag "k8s.io/component-base/cli/flag"
-	"k8s.io/component-base/cli/globalflag"
-	"k8s.io/klog"
+    "github.com/spf13/cobra"
+    cliflag "k8s.io/component-base/cli/flag"
+    "k8s.io/component-base/cli/globalflag"
+    "k8s.io/component-base/term"
+    "k8s.io/klog"
 
-	"github.com/kubeedge/kubeedge/cloud/cmd/csidriver/app/options"
-	"github.com/kubeedge/kubeedge/cloud/pkg/csidriver"
-	"github.com/kubeedge/kubeedge/pkg/util/flag"
-	"github.com/kubeedge/kubeedge/pkg/version"
-	"github.com/kubeedge/kubeedge/pkg/version/verflag"
+    "github.com/kubeedge/kubeedge/cloud/cmd/csidriver/app/options"
+    "github.com/kubeedge/kubeedge/cloud/pkg/csidriver"
+    "github.com/kubeedge/kubeedge/pkg/util/flag"
+    "github.com/kubeedge/kubeedge/pkg/version"
+    "github.com/kubeedge/kubeedge/pkg/version/verflag"
 )
 
 func NewCSIDriverCommand() *cobra.Command {
-	opts := options.NewCSIDriverOptions()
-	cmd := &cobra.Command{
-		Use: "csidriver",
-		Long: `CSI Driver from KubeEdge: this is more like CSI Driver proxy,
+    opts := options.NewCSIDriverOptions()
+    cmd := &cobra.Command{
+        Use: "csidriver",
+        Long: `CSI Driver from KubeEdge: this is more like CSI Driver proxy,
 		and it implements all of the CSI Identity and Controller interfaces.
 		It sends messages to CloudHub which will forward to edge. Actually all of the actions
 		about the Volume Lifecycle are executed in the CSI Driver from Vendor at edge`,
-		Run: func(cmd *cobra.Command, args []string) {
-			verflag.PrintAndExitIfRequested()
-			flag.PrintFlags(cmd.Flags())
+        Run: func(cmd *cobra.Command, args []string) {
+            verflag.PrintAndExitIfRequested()
+            flag.PrintFlags(cmd.Flags())
 
-			// To help debugging, immediately log version
-			klog.Infof("Version: %+v", version.Get())
+            // To help debugging, immediately log version
+            klog.Infof("Version: %+v", version.Get())
 
-			// start driver
-			driver, err := csidriver.NewCSIDriver(opts)
-			if err != nil {
-				klog.Errorf("failed to initialize driver: %s", err.Error())
-				return
-			}
-			driver.Run()
-		},
-	}
-	fs := cmd.Flags()
-	namedFs := opts.Flags()
-	verflag.AddFlags(namedFs.FlagSet("global"))
-	globalflag.AddGlobalFlags(namedFs.FlagSet("global"), cmd.Name())
-	for _, f := range namedFs.FlagSets {
-		fs.AddFlagSet(f)
-	}
+            // start driver
+            driver, err := csidriver.NewCSIDriver(opts)
+            if err != nil {
+                klog.Errorf("failed to initialize driver: %s", err.Error())
+                return
+            }
+            driver.Run()
+        },
+    }
+    fs := cmd.Flags()
+    namedFs := opts.Flags()
+    verflag.AddFlags(namedFs.FlagSet("global"))
+    globalflag.AddGlobalFlags(namedFs.FlagSet("global"), cmd.Name())
+    for _, f := range namedFs.FlagSets {
+        fs.AddFlagSet(f)
+    }
 
-	usageFmt := "Usage:\n  %s\n"
-	cols, _, _ := term.TerminalSize(cmd.OutOrStdout())
-	cmd.SetUsageFunc(func(cmd *cobra.Command) error {
-		fmt.Fprintf(cmd.OutOrStderr(), usageFmt, cmd.UseLine())
-		cliflag.PrintSections(cmd.OutOrStderr(), namedFs, cols)
-		return nil
-	})
-	cmd.SetHelpFunc(func(cmd *cobra.Command, args []string) {
-		fmt.Fprintf(cmd.OutOrStdout(), "%s\n\n"+usageFmt, cmd.Long, cmd.UseLine())
-		cliflag.PrintSections(cmd.OutOrStdout(), namedFs, cols)
-	})
-	return cmd
+    usageFmt := "Usage:\n  %s\n"
+    cols, _, _ := term.TerminalSize(cmd.OutOrStdout())
+    cmd.SetUsageFunc(func(cmd *cobra.Command) error {
+        fmt.Fprintf(cmd.OutOrStderr(), usageFmt, cmd.UseLine())
+        cliflag.PrintSections(cmd.OutOrStderr(), namedFs, cols)
+        return nil
+    })
+    cmd.SetHelpFunc(func(cmd *cobra.Command, args []string) {
+        fmt.Fprintf(cmd.OutOrStdout(), "%s\n\n"+usageFmt, cmd.Long, cmd.UseLine())
+        cliflag.PrintSections(cmd.OutOrStdout(), namedFs, cols)
+    })
+    return cmd
 }
