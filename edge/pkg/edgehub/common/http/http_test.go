867b404df20453bab81a6b474a536c45aba8803c
Jakub Guzik <jakubmguzik@gmail.com>
2021-02-11 22:46:31 +0100

    Ut coverage in edge/pkg/edgehyb/common/http/
    Signed-off-by: Jakub Guzik <jakubmguzik@gmail.com>

--- edge/pkg/edgehub/common/http/http_test.go
+++ edge/pkg/edgehub/common/http/http_test.go
@@ -17,8 +17,11 @@ limitations under the License.
 package http
 
 import (
+	"bytes"
 	"crypto/tls"
 	"crypto/x509"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"reflect"
 	"testing"
@@ -29,6 +32,8 @@ import (
 const (
 	CertFile = "/tmp/kubeedge/testData/edge.crt"
 	KeyFile  = "/tmp/kubeedge/testData/edge.key"
+	Method   = "GET"
+	Url      = "kubeedge.io"
 )
 
 //TestNewHttpClient() tests the creation of a new HTTP client
@@ -103,5 +108,170 @@ func TestNewHTTPSClient(t *testing.T) {
 				t.Errorf("NewHTTPSClient() = %v, want %v", got, tt.want)
 			}
 		})
+	}
+}
+
+//TestNewHTTPClientWithCA() tests the creation of a new HTTP using filled capem
+func TestNewHTTPClientWithCA(t *testing.T) {
+	err := util.GenerateTestCertificate("/tmp/kubeedge/testData/", "edge", "edge")
+	if err != nil {
+		t.Errorf("Error in generating fake certificates: %v", err)
+		return
+	}
+	capem, err := ioutil.ReadFile(CertFile)
+	if err != nil {
+		t.Errorf("Error in loading Cert file: %v", err)
+		return
+	}
+	certificate := tls.Certificate{}
+
+	testPool := x509.NewCertPool()
+	if ok := testPool.AppendCertsFromPEM(capem); !ok {
+		t.Errorf("cannot parse the certificates")
+		return
+	}
+
+	type args struct {
+		capem       []byte
+		certificate tls.Certificate
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *http.Client
+		wantErr bool
+	}{
+		{
+			name: "TestNewHTTPClientWithCA: ",
+			args: args{
+				capem:       capem,
+				certificate: certificate,
+			},
+			want: &http.Client{
+				Transport: &http.Transport{
+					TLSClientConfig: &tls.Config{
+						RootCAs:            testPool,
+						InsecureSkipVerify: false,
+						Certificates:       []tls.Certificate{certificate},
+					},
+				},
+				Timeout: connectTimeout,
+			},
+			wantErr: false,
+		},
+		{
+			name: "Wrong certifcate given when getting HTTP client",
+			args: args{
+				capem:       []byte{},
+				certificate: certificate,
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewHTTPClientWithCA(tt.args.capem, tt.args.certificate)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewHTTPClientWithCA() error = %v, expectedError = %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewHTTPClientWithCA() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+//TestBuildRequest() tests the process of message building
+func TestBuildRequest(t *testing.T) {
+	reader := bytes.NewReader([]byte{})
+	token := "token"
+	nodeName := "name"
+
+	req, err := http.NewRequest(Method, Url, reader)
+	if err != nil {
+		t.Errorf("Error in creating new http request message: %v", err)
+		return
+	}
+	req.Header.Add("Authorization", "Bearer "+token)
+	req.Header.Add("NodeName", nodeName)
+
+	type args struct {
+		method   string
+		urlStr   string
+		body     io.Reader
+		token    string
+		nodeName string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *http.Request
+		wantErr bool
+	}{
+		{
+			name: "TestBuildRequest: ",
+			args: args{
+				method:   Method,
+				urlStr:   Url,
+				body:     reader,
+				token:    token,
+				nodeName: nodeName,
+			},
+			want:    req,
+			wantErr: false,
+		},
+		{
+			name: "NewRequest failure causes BuildRequest failure: ",
+			args: args{
+				method:   "INVALID\n",
+				urlStr:   Url,
+				body:     reader,
+				token:    token,
+				nodeName: nodeName,
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := BuildRequest(tt.args.method, tt.args.urlStr, tt.args.body, tt.args.token, tt.args.nodeName)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("BuildRequest() error = %v, expectedError = %v", err, tt.wantErr)
+				return
+			}
+			//needed to handle failure testcase because can't deep compare field in nil
+			if got == tt.want && err != nil && tt.wantErr == true {
+				return
+			}
+			if !reflect.DeepEqual(got.Header, tt.want.Header) {
+				t.Errorf("BuildRequest() Header = %v, want %v", got, tt.want.Header)
+			}
+			if !reflect.DeepEqual(got.Body, tt.want.Body) {
+				t.Errorf("BuildRequest() Body = %v, want %v", got, tt.want.Body)
+			}
+		})
+	}
+}
+
+//TestSendRequestFailure() uses fake data and expects function to fail
+func TestSendRequestFailure(t *testing.T) {
+	httpClient := NewHTTPClient()
+	if httpClient == nil {
+		t.Fatal("Failed to build HTTP client")
+	}
+
+	req, err := http.NewRequest(Method, Url, bytes.NewReader([]byte{}))
+	if err != nil {
+		t.Errorf("Error in creating new http request message: %v", err)
+		return
+	}
+
+	resp, respErr := SendRequest(req, httpClient)
+	if resp != nil && respErr == nil {
+		t.Errorf("Error, response should not come as data is not valid")
+		return
 	}
 }
