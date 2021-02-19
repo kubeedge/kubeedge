d064d944d8ac47de7af9e2f6bc51037dd4b03374
Elaine4CY <chenyuncy2017@outlook.com>
2021-02-19 17:27:04 +0800

    add servicebus
    Signed-off-by: WintonChan <cw_hao@163.com>
    Signed-off-by: Elaine4CY <chenyuncy2017@outlook.com>

--- cloud/pkg/router/constants/constants.go
+++ cloud/pkg/router/constants/constants.go
@@ -7,4 +7,6 @@ const (
 	EventbusEndpoint string = "eventbus"
 	GroupResource    string = "resource"
 	MaxmMessageBytes int64  = 1.2e+7
+	ServicebusEndpoint string = "servicebus"
+	ServicebusProvider string = "servicebus"
 )
