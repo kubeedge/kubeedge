7ac16cb62684a1a94166f52246c3e063a04d1185
qugq0228 <1341389014@qq.com>
2021-06-04 15:52:22 +0800

    fix Can not find apiServer connection id in Tunnel session
    Signed-off-by: qugq0228 <1341389014@qq.com>

--- cloud/pkg/cloudstream/containermetrics_connection.go
+++ cloud/pkg/cloudstream/containermetrics_connection.go
@@ -114,8 +114,8 @@ func (ms *ContainerMetricsConnection) Serve() error {
 			klog.Infof("%s send close message to edge successfully", ms.String())
 			return nil
 		case <-ms.EdgePeerDone():
-			klog.Infof("%s find edge peer done, so stop this connection", ms.String())
-			return nil
+			err := fmt.Errorf("%s find edge peer done, so stop this connection", ms.String())
+			return err
 		}
 	}
 }
