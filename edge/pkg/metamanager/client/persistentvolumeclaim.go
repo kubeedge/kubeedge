d82c4086ac6a91049b3757c36ad5815cb749ed67
gy95 <guoyao17@huawei.com>
2021-08-12 16:01:53 +0800

    simplify the way of getting message []byte format
    Signed-off-by: gy95 <guoyao17@huawei.com>

--- edge/pkg/metamanager/client/persistentvolumeclaim.go
+++ edge/pkg/metamanager/client/persistentvolumeclaim.go
@@ -58,15 +58,9 @@ func (c *persistentvolumeclaims) Get(name string, options metav1.GetOptions) (*a
 		return nil, fmt.Errorf("get persistentvolumeclaim from metaManager failed, err: %v", err)
 	}
 
-	var content []byte
-	switch msg.Content.(type) {
-	case []byte:
-		content = msg.GetContent().([]byte)
-	default:
-		content, err = json.Marshal(msg.GetContent())
-		if err != nil {
-			return nil, fmt.Errorf("marshal message to persistentvolumeclaim failed, err: %v", err)
-		}
+	content, err := msg.GetContentData()
+	if err != nil {
+		return nil, fmt.Errorf("parse message to persistentvolumeclaim failed, err: %v", err)
 	}
 
 	if msg.GetOperation() == model.ResponseOperation && msg.GetSource() == metamanager.MetaManagerModuleName {
