76aa40aad75fee5f9f997d03db4db8975b83ee90
gy95 <guoyao17@huawei.com>
2021-08-13 17:13:54 +0800

    update k8s version to v1.21.4 Signed-off-by: gy95 <guoyao17@huawei.com>

--- vendor/github.com/karrick/godirwalk/readdir_unix.go
+++ vendor/github.com/karrick/godirwalk/readdir_unix.go
@@ -8,7 +8,19 @@ import (
 	"unsafe"
 )
 
-func readdirents(osDirname string, scratchBuffer []byte) (Dirents, error) {
+// MinimumScratchBufferSize specifies the minimum size of the scratch buffer
+// that ReadDirents, ReadDirnames, Scanner, and Walk will use when reading file
+// entries from the operating system. During program startup it is initialized
+// to the result from calling `os.Getpagesize()` for non Windows environments,
+// and 0 for Windows.
+var MinimumScratchBufferSize = os.Getpagesize()
+
+func newScratchBuffer() []byte { return make([]byte, MinimumScratchBufferSize) }
+
+func readDirents(osDirname string, scratchBuffer []byte) ([]*Dirent, error) {
+	var entries []*Dirent
+	var workBuffer []byte
+
 	dh, err := os.Open(osDirname)
 	if err != nil {
 		return nil, err
@@ -16,55 +28,59 @@ func readdirents(osDirname string, scratchBuffer []byte) (Dirents, error) {
 	fd := int(dh.Fd())
 
 	if len(scratchBuffer) < MinimumScratchBufferSize {
-		scratchBuffer = make([]byte, DefaultScratchBufferSize)
+		scratchBuffer = newScratchBuffer()
 	}
 
-	var entries Dirents
-	var de *syscall.Dirent
-
+	var sde syscall.Dirent
 	for {
-		n, err := syscall.ReadDirent(fd, scratchBuffer)
-		if err != nil {
-			_ = dh.Close() // ignore potential error returned by Close
-			return nil, err
-		}
-		if n <= 0 {
-			break // end of directory reached
-		}
-		// Loop over the bytes returned by reading the directory entries.
-		buf := scratchBuffer[:n]
-		for len(buf) > 0 {
-			de = (*syscall.Dirent)(unsafe.Pointer(&buf[0])) // point entry to first syscall.Dirent in buffer
-			buf = buf[de.Reclen:]                           // advance buffer for next iteration through loop
-
-			if inoFromDirent(de) == 0 {
-				continue // this item has been deleted, but its entry not yet removed from directory listing
-			}
-
-			nameSlice := nameFromDirent(de)
-			namlen := len(nameSlice)
-			if (namlen == 0) || (namlen == 1 && nameSlice[0] == '.') || (namlen == 2 && nameSlice[0] == '.' && nameSlice[1] == '.') {
-				continue // skip unimportant entries
-			}
-			osChildname := string(nameSlice)
-
-			mode, err := modeType(de, osDirname, osChildname)
+		if len(workBuffer) == 0 {
+			n, err := syscall.ReadDirent(fd, scratchBuffer)
+			// n, err := unix.ReadDirent(fd, scratchBuffer)
 			if err != nil {
-				_ = dh.Close() // ignore potential error returned by Close
+				if err == syscall.EINTR /* || err == unix.EINTR */ {
+					continue
+				}
+				_ = dh.Close()
 				return nil, err
 			}
-
-			entries = append(entries, &Dirent{name: osChildname, modeType: mode})
+			if n <= 0 { // end of directory: normal exit
+				if err = dh.Close(); err != nil {
+					return nil, err
+				}
+				return entries, nil
+			}
+			workBuffer = scratchBuffer[:n] // trim work buffer to number of bytes read
 		}
-	}
 
-	if err = dh.Close(); err != nil {
-		return nil, err
+		copy((*[unsafe.Sizeof(syscall.Dirent{})]byte)(unsafe.Pointer(&sde))[:], workBuffer)
+		workBuffer = workBuffer[reclen(&sde):] // advance buffer for next iteration through loop
+
+		if inoFromDirent(&sde) == 0 {
+			continue // inode set to 0 indicates an entry that was marked as deleted
+		}
+
+		nameSlice := nameFromDirent(&sde)
+		nameLength := len(nameSlice)
+
+		if nameLength == 0 || (nameSlice[0] == '.' && (nameLength == 1 || (nameLength == 2 && nameSlice[1] == '.'))) {
+			continue
+		}
+
+		childName := string(nameSlice)
+		mt, err := modeTypeFromDirent(&sde, osDirname, childName)
+		if err != nil {
+			_ = dh.Close()
+			return nil, err
+		}
+		entries = append(entries, &Dirent{name: childName, path: osDirname, modeType: mt})
 	}
-	return entries, nil
 }
 
-func readdirnames(osDirname string, scratchBuffer []byte) ([]string, error) {
+func readDirnames(osDirname string, scratchBuffer []byte) ([]string, error) {
+	var entries []string
+	var workBuffer []byte
+	var sde *syscall.Dirent
+
 	dh, err := os.Open(osDirname)
 	if err != nil {
 		return nil, err
@@ -72,44 +88,44 @@ func readdirnames(osDirname string, scratchBuffer []byte) ([]string, error) {
 	fd := int(dh.Fd())
 
 	if len(scratchBuffer) < MinimumScratchBufferSize {
-		scratchBuffer = make([]byte, DefaultScratchBufferSize)
+		scratchBuffer = newScratchBuffer()
 	}
-
-	var entries []string
-	var de *syscall.Dirent
 
 	for {
-		n, err := syscall.ReadDirent(fd, scratchBuffer)
-		if err != nil {
-			_ = dh.Close() // ignore potential error returned by Close
-			return nil, err
-		}
-		if n <= 0 {
-			break // end of directory reached
-		}
-		// Loop over the bytes returned by reading the directory entries.
-		buf := scratchBuffer[:n]
-		for len(buf) > 0 {
-			de = (*syscall.Dirent)(unsafe.Pointer(&buf[0])) // point entry to first syscall.Dirent in buffer
-			buf = buf[de.Reclen:]                           // advance buffer for next iteration through loop
-
-			if inoFromDirent(de) == 0 {
-				continue // this item has been deleted, but its entry not yet removed from directory listing
+		if len(workBuffer) == 0 {
+			n, err := syscall.ReadDirent(fd, scratchBuffer)
+			// n, err := unix.ReadDirent(fd, scratchBuffer)
+			if err != nil {
+				if err == syscall.EINTR /* || err == unix.EINTR */ {
+					continue
+				}
+				_ = dh.Close()
+				return nil, err
 			}
-
-			nameSlice := nameFromDirent(de)
-			namlen := len(nameSlice)
-			if (namlen == 0) || (namlen == 1 && nameSlice[0] == '.') || (namlen == 2 && nameSlice[0] == '.' && nameSlice[1] == '.') {
-				continue // skip unimportant entries
+			if n <= 0 { // end of directory: normal exit
+				if err = dh.Close(); err != nil {
+					return nil, err
+				}
+				return entries, nil
 			}
-			osChildname := string(nameSlice)
-
-			entries = append(entries, osChildname)
+			workBuffer = scratchBuffer[:n] // trim work buffer to number of bytes read
 		}
-	}
 
-	if err = dh.Close(); err != nil {
-		return nil, err
+		sde = (*syscall.Dirent)(unsafe.Pointer(&workBuffer[0])) // point entry to first syscall.Dirent in buffer
+		// Handle first entry in the work buffer.
+		workBuffer = workBuffer[reclen(sde):] // advance buffer for next iteration through loop
+
+		if inoFromDirent(sde) == 0 {
+			continue // inode set to 0 indicates an entry that was marked as deleted
+		}
+
+		nameSlice := nameFromDirent(sde)
+		nameLength := len(nameSlice)
+
+		if nameLength == 0 || (nameSlice[0] == '.' && (nameLength == 1 || (nameLength == 2 && nameSlice[1] == '.'))) {
+			continue
+		}
+
+		entries = append(entries, string(nameSlice))
 	}
-	return entries, nil
 }
--- vendor/k8s.io/kubernetes/pkg/kubelet/dockershim/helpers_linux.go
+++ vendor/k8s.io/kubernetes/pkg/kubelet/dockershim/helpers_linux.go
@@ -34,6 +34,7 @@ import (
 	runtimeapi "k8s.io/cri-api/pkg/apis/runtime/v1alpha2"
 )
 
+// DefaultMemorySwap always returns 0 for no memory swap in a sandbox
 func DefaultMemorySwap() int64 {
 	return 0
 }
@@ -117,6 +118,8 @@ func (ds *dockerService) updateCreateConfig(
 				CPUShares:  rOpts.CpuShares,
 				CPUQuota:   rOpts.CpuQuota,
 				CPUPeriod:  rOpts.CpuPeriod,
+				CpusetCpus: rOpts.CpusetCpus,
+				CpusetMems: rOpts.CpusetMems,
 			}
 			createConfig.HostConfig.OomScoreAdj = int(rOpts.OomScoreAdj)
 		}
@@ -151,8 +154,4 @@ func getNetworkNamespace(c *dockertypes.ContainerJSON) (string, error) {
 		return "", fmt.Errorf("cannot find network namespace for the terminated container %q", c.ID)
 	}
 	return fmt.Sprintf(dockerNetNSFmt, c.State.Pid), nil
-}
-
-// applyExperimentalCreateConfig applys experimental configures from sandbox annotations.
-func applyExperimentalCreateConfig(createConfig *dockertypes.ContainerCreateConfig, annotations map[string]string) {
 }
