be159edbb3964bb2b182fc76badf039d35136207
gy95 <guoyao17@huawei.com>
2021-09-06 21:14:31 +0800

    add unit test for package validation
    Signed-off-by: gy95 <guoyao17@huawei.com>

--- pkg/util/validation/validation_test.go
+++ pkg/util/validation/validation_test.go
@@ -1,6 +1,9 @@
 package validation
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestIsValidIP(t *testing.T) {
 	cases := []struct {
@@ -14,22 +17,22 @@ func TestIsValidIP(t *testing.T) {
 			Expect: true,
 		},
 		{
-			Name:   "unvalid have port",
+			Name:   "invalid have port",
 			IP:     "1.1.1.1:1234",
 			Expect: false,
 		},
 		{
-			Name:   "unvalid ip1",
+			Name:   "invalid ip1",
 			IP:     "1.1.1.",
 			Expect: false,
 		},
 		{
-			Name:   "unvalid unit socket",
+			Name:   "invalid unit socket",
 			IP:     "unix:///var/run/docker.sock",
 			Expect: false,
 		},
 		{
-			Name:   "unvalid http",
+			Name:   "invalid http",
 			IP:     "http://127.0.0.1",
 			Expect: false,
 		},
@@ -43,5 +46,51 @@ func TestIsValidIP(t *testing.T) {
 				t.Errorf("Input %s Expect %v while get %v", c.IP, c.Expect, v)
 			}
 		})
+	}
+}
+
+func TestIsValidPortNum(t *testing.T) {
+	cases := []struct {
+		Name   string
+		Port   int
+		Expect []string
+	}{
+		{
+			Name:   "invalid port",
+			Port:   0,
+			Expect: []string{"must be between 1 and 65535, inclusive"},
+		},
+		{
+			Name:   "valid port",
+			Port:   1,
+			Expect: nil,
+		},
+		{
+			Name:   "valid port",
+			Port:   65535,
+			Expect: nil,
+		},
+		{
+			Name:   "invalid port",
+			Port:   65536,
+			Expect: []string{"must be between 1 and 65535, inclusive"},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.Name, func(t *testing.T) {
+			v := IsValidPortNum(c.Port)
+			if !reflect.DeepEqual(v, c.Expect) {
+				t.Errorf("Input %d Expect %v while get %v", c.Port, c.Expect, v)
+			}
+		})
+	}
+}
+
+func TestInclusiveRangeError(t *testing.T) {
+	result := InclusiveRangeError(1, 65535)
+	expect := "must be between 1 and 65535, inclusive"
+	if !reflect.DeepEqual(result, expect) {
+		t.Errorf("Expected %v while get %v", expect, result)
 	}
 }
