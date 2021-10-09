773b037ec6b053823868ba1fec3fec3154da4972
gy95 <guoyao17@huawei.com>
2021-10-09 14:48:45 +0800

    remove duplicate SessionHostNameOverride constant
    Signed-off-by: gy95 <guoyao17@huawei.com>

--- pkg/stream/constants.go
+++ pkg/stream/constants.go
@@ -3,7 +3,4 @@ package stream
 const (
 	SessionKeyHostNameOverride = "SessionHostNameOverride"
 	SessionKeyInternalIP       = "SessionInternalIP"
-
-	// TODO: Fix SessionHostNameOverride typo, remove this in v1.7.x
-	SessionKeyHostNameOverrideOld = "SessionHostNameOverride"
 )
