2c8cc7956396883bad64ae4e61ce7ebaead9da48
xiawei.xu <xiawei.xu@daocloud.io>
2021-11-09 14:40:32 +0800

    fixed error message capitalization
    Signed-off-by: xiawei.xu <xiawei.xu@daocloud.io>

--- edge/pkg/devicetwin/process.go
+++ edge/pkg/devicetwin/process.go
@@ -40,11 +40,10 @@ func (dt *DeviceTwin) RegisterDTModule(name string) {
 }
 
 //distributeMsg distribute message to diff module
-func (dt *DeviceTwin) distributeMsg(m interface{}) error {
+func (dt *DeviceTwin) gitdistributeMsg(m interface{}) error {
 	msg, ok := m.(model.Message)
 	if !ok {
-		klog.Errorf("Distribute message, msg is nil")
-		return errors.New("Distribute message, msg is nil")
+		return errors.New("distribute message, msg is nil")
 	}
 	message := dttype.DTMessage{Msg: &msg}
 	if message.Msg.GetParentID() != "" {
