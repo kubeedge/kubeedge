34ea3fc81131fc5a53655d53d689cfdf1d8af9b5
zhu733756 <zhu733756@kubesphere.io>
2021-12-11 14:11:05 +0800

    optimize the iptables manager
    Signed-off-by: zhu733756 <zhu733756@kubesphere.io>

--- cloud/cmd/cloudcore/app/server.go
+++ cloud/cmd/cloudcore/app/server.go
@@ -91,16 +91,17 @@ kubernetes controller which manages devices so that the device metadata/status d
 
 			registerModules(config)
 
+			ctx := beehiveContext.GetContext()
 			if config.Modules.IptablesManager == nil || config.Modules.IptablesManager.Enable && config.Modules.IptablesManager.Mode == v1alpha1.InternalMode {
 				// By default, IptablesManager manages tunnel port related iptables rules
 				// The internal mode will share the host network, forward to the stream port.
 				streamPort := int(config.Modules.CloudStream.StreamPort)
-				go iptables.NewIptablesManager(streamPort).Run()
+				go iptables.NewIptablesManager(config.KubeAPIConfig, streamPort).Run(ctx)
 			}
 
 			// Start all modules
 			core.StartModules()
-			gis.Start(beehiveContext.Done())
+			gis.Start(ctx.Done())
 			core.GracefulShutdown()
 		},
 	}
