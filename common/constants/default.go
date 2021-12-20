acf4e4459377937bcc3213611af25fc7a884d2ed
gy95 <guoyao17@huawei.com>
2021-12-20 16:02:49 +0800

    update k8s version to v1.21.8
    Signed-off-by: gy95 <guoyao17@huawei.com>

--- common/constants/default.go
+++ common/constants/default.go
@@ -64,7 +64,7 @@ const (
 	DefaultVolumeStatsAggPeriod        = time.Minute
 	DefaultTunnelPort                  = 10004
 
-	CurrentSupportK8sVersion = "v1.19.3"
+	CurrentSupportK8sVersion = "v1.21.4"
 
 	// MetaManager
 	DefaultPodStatusSyncInterval = 60
