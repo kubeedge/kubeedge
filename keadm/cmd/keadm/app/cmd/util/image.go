30a8a01649a03a830a9970a763ceb33714d8a687
gy95 <guoyao17@huawei.com>
2022-03-16 11:56:45 +0800

    fix golangcilint
    Signed-off-by: gy95 <guoyao17@huawei.com>

--- keadm/cmd/keadm/app/cmd/util/image.go
+++ keadm/cmd/keadm/app/cmd/util/image.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2022 The KubeEdge Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package util
 
 import (
@@ -87,8 +103,12 @@ func (runtime *DockerRuntime) PullImages(images []string) error {
 			return err
 		}
 
-		io.Copy(io.Discard, rc)
-		rc.Close()
+		if _, err := io.Copy(io.Discard, rc); err != nil {
+			return err
+		}
+		if err := rc.Close(); err != nil {
+			return err
+		}
 		fmt.Printf("Successfully pulled %s\n", image)
 	}
 
@@ -195,7 +215,6 @@ func (runtime *CRIRuntime) PullImages(images []string) error {
 // CopyResources copies binary and configuration file from the image to the host.
 // The same way as func (runtime *DockerRuntime) CopyResources
 func (runtime *CRIRuntime) CopyResources(edgeImage string, dirs map[string]string, files map[string]string) error {
-
 	psc := &runtimeapi.PodSandboxConfig{
 		Metadata: &runtimeapi.PodSandboxMetadata{Name: KubeEdgeBinaryName},
 	}
