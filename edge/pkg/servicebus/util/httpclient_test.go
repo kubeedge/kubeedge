d1a280516b0fc4df79d7b82f6ce069692eea3855
jiawei <jiawei.liu@daocloud.io>
2022-06-17 10:53:49 +0800

    improve edge/pkg/servicebus/util test coverage
    Signed-off-by: jiawei <jiawei.liu@daocloud.io>

--- edge/pkg/servicebus/util/httpclient_test.go
+++ edge/pkg/servicebus/util/httpclient_test.go
@@ -1,42 +1,103 @@
 package util
 
 import (
+	"crypto/tls"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 func TestURLClient_HTTPDo(t *testing.T) {
-	client, err := GetURLClient(nil)
-	if err != nil {
-		t.Errorf("GetURLClient error: %v", err)
+
+	type args struct {
+		option *URLClientOption
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "TestURLClient_HTTPDo(): Case 1: SuccessCase",
+			args: args{
+				&URLClientOption{
+					SSLEnabled:            true,
+					Compressed:            true,
+					HandshakeTimeout:      0,
+					ResponseHeaderTimeout: 0,
+					TLSConfig:             &tls.Config{InsecureSkipVerify: true},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "TestURLClient_HTTPDo(): Case 2: SuccessCase",
+			args: args{
+				&URLClientOption{
+					SSLEnabled:            false,
+					Compressed:            true,
+					HandshakeTimeout:      0,
+					ResponseHeaderTimeout: 0,
+					TLSConfig:             nil,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "TestURLClient_HTTPDo(): Case 3: SuccessCase",
+			args: args{
+				nil,
+			},
+			wantErr: false,
+		}, {
+			name: "TestURLClient_HTTPDo(): Case 4: FailureCase",
+			args: args{
+				&URLClientOption{
+					SSLEnabled:            true,
+					Compressed:            true,
+					HandshakeTimeout:      0,
+					ResponseHeaderTimeout: 0,
+					TLSConfig:             nil,
+				},
+			},
+			wantErr: true,
+		},
 	}
 
-	ts := getMockServer(t)
-	resp, err := client.HTTPDo("GET", ts.URL+"/test", http.Header{}, nil)
-	if err != nil {
-		t.Errorf("HTTPDo error: %v", err)
-	} else {
-		defer resp.Body.Close()
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		t.Errorf("got error status code, resp is %v", resp)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			client, err := GetURLClient(tt.args.option)
+			if err != nil {
+				t.Errorf("GetURLClient() error: %v", err)
+			}
+			ts := getMockHttpServer(t, tt.args.option)
+			resp, err := client.HTTPDo("GET", ts.URL+"/test", nil, nil)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("HTTPDo() error = %v, expectedError = %v", err, tt.wantErr)
+			} else if err == nil {
+				defer resp.Body.Close()
+			}
+		})
 	}
 }
 
-func getMockServer(t *testing.T) *httptest.Server {
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func getMockHttpServer(t *testing.T, option *URLClientOption) *httptest.Server {
+	ts := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case "GET":
+			w.WriteHeader(http.StatusOK)
 			if r.URL.EscapedPath() != "/test" {
-				t.Errorf("path error: %s", r.URL.EscapedPath())
+				t.Errorf("Path error: %s", r.URL.EscapedPath())
 				w.WriteHeader(http.StatusNotFound)
 			} else {
 				w.WriteHeader(http.StatusOK)
 			}
 		}
 	}))
-
+	if option != nil && option.SSLEnabled {
+		ts.StartTLS()
+	} else {
+		ts.Start()
+	}
 	return ts
 }
