21909a1abf0466d36b0caa11c80f6d6530edaba8
wackxu <xushiwei5@huawei.com>
2022-10-20 17:19:40 +0800

    fix failed quic e2e
    Signed-off-by: wackxu <xushiwei5@huawei.com>

--- cloud/pkg/router/provider/servicebus/servicebus.go
+++ cloud/pkg/router/provider/servicebus/servicebus.go
@@ -87,11 +87,12 @@ func (sb *ServiceBus) Forward(target provider.Target, data interface{}) (respons
 		return nil, fmt.Errorf("message type %T error", data)
 	}
 	res := make(map[string]interface{})
-	v, ok := message.Content.(string)
+	content, err := message.GetContentData()
 	if !ok {
-		return nil, errors.New("message content invalid convert to string")
+		klog.Errorf("get message %s content err: %v", message.GetID(), err)
+		return nil, fmt.Errorf("get message %s contant err: %v", message.GetID(), err)
 	}
-	res["data"] = []byte(v)
+	res["data"] = content
 	resp, err := target.GoToTarget(res, nil)
 	if err != nil {
 		klog.Errorf("message is send to target failed. msgID: %s, target: %s, err:%v", message.GetID(), target.Name(), err)
