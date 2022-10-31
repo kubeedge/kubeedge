54eb9b4d2b797901f32f747a0d7fd53e0ce12a26
xin.li <xin.li@daocloud.io>
2022-10-31 21:39:24 +0800

    improve  example output about kubeedge
    Signed-off-by: xin.li <xin.li@daocloud.io>

--- keadm/cmd/keadm/app/cmd/cloud/init.go
+++ keadm/cmd/keadm/app/cmd/cloud/init.go
@@ -40,7 +40,7 @@ If not installed, please install the Kubernetes first.
 keadm init
 - This command will render and install the Charts for Kubeedge cloud component
 
-keadm init --advertise-address=127.0.0.1 --profile version=v1.9.0 --kube-config=/root/.kube/config
+keadm init --advertise-address=127.0.0.1 --profile version=v%s --kube-config=/root/.kube/config
   - kube-config is the absolute path of kubeconfig which used to secure connectivity between cloudcore and kube-apiserver
 	- a list of helm style set flags like "--set key=value" can be implemented, ref: https://github.com/kubeedge/kubeedge/tree/master/manifests/charts/cloudcore/README.md
 `
@@ -57,7 +57,7 @@ func NewCloudInit() *cobra.Command {
 		Use:     "init",
 		Short:   "Bootstraps cloud component. Checks and install (if required) the pre-requisites.",
 		Long:    cloudInitLongDescription,
-		Example: cloudInitExample,
+		Example: fmt.Sprintf(cloudInitExample, types.DefaultKubeEdgeVersion),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			checkFlags := func(f *pflag.Flag) {
 				util.AddToolVals(f, flagVals)
