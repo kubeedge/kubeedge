054b8533f8d4c17ccb02cdaa577db5371513bb69
xian-jie.shen <327411586@qq.com>
2022-12-25 16:20:52 +0800

    messagetype
    Signed-off-by: xian-jie.shen <327411586@qq.com>

--- pkg/stream/message.go
+++ pkg/stream/message.go
@@ -41,6 +41,8 @@ func (m MessageType) String() string {
 		return "DATA"
 	case MessageTypeRemoveConnect:
 		return "REMOVE_CONNECT"
+	case MessageTypeCloseConnect:
+		return "CLOSE_CONNECT"
 	}
 	return "UNKNOWN"
 }
