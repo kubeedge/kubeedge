a7a7ab75465b9f9760fba46e16c3482375f6b397
wuxuesong <xuesongwu@yunify.com>
2023-01-13 18:11:32 +0800

    feat: cloudhub support rsa certs
    Signed-off-by: wuxuesong <xuesongwu@yunify.com>

--- cloud/pkg/cloudhub/servers/httpserver/server.go
+++ cloud/pkg/cloudhub/servers/httpserver/server.go
@@ -223,10 +223,14 @@ func signCerts(subInfo pkix.Name, pbKey crypto.PublicKey, usages []x509.ExtKeyUs
 		return nil, fmt.Errorf("unable to ParseCertificate: %v", err)
 	}
 
+	var caKey crypto.Signer
 	caKeyDER := hubconfig.Config.CaKey
-	caKey, err := x509.ParseECPrivateKey(caKeyDER)
+	caKey, err = x509.ParseECPrivateKey(caKeyDER)
 	if err != nil {
-		return nil, fmt.Errorf("unable to ParseECPrivateKey: %v", err)
+		caKey, err = x509.ParsePKCS1PrivateKey(caKeyDER)
+		if err != nil {
+			return nil, fmt.Errorf("unable to Parse PrivateKey: %v", err)
+		}
 	}
 
 	edgeCertSigningDuration := hubconfig.Config.CloudHub.EdgeCertSigningDuration
