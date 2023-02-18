5e993e60640a63a9146c3431d268c5cb67c5748a
xin.li <xin.li@daocloud.io>
2023-02-18 12:32:02 +0800

    [UT] improve args definition for util_test
    Signed-off-by: xin.li <xin.li@daocloud.io>

--- pkg/metaserver/util/util_test.go
+++ pkg/metaserver/util/util_test.go
@@ -17,39 +17,34 @@ import (
 )
 
 func TestGetMessageAPIVerison(t *testing.T) {
-	type args struct {
-		msg *beehiveModel.Message
-	}
 	tests := []struct {
 		name string
-		args args
+		msg  *beehiveModel.Message
 		want string
 	}{
 		{
 			name: "TestGetMessageAPIVerison(): Case 1: Content pod",
-			args: args{
-				msg: &beehiveModel.Message{
-					Content: &corev1.Pod{
-						TypeMeta: metav1.TypeMeta{
-							Kind:       "Pod",
-							APIVersion: "v1",
-						},
+			msg: &beehiveModel.Message{
+				Content: &corev1.Pod{
+					TypeMeta: metav1.TypeMeta{
+						Kind:       "Pod",
+						APIVersion: "v1",
 					},
-				}},
+				},
+			},
 			want: "v1",
 		},
 		{
 			name: "TestGetMessageAPIVerison(): Case 2: Content other",
-			args: args{
-				msg: &beehiveModel.Message{
-					Content: "content",
-				}},
+			msg: &beehiveModel.Message{
+				Content: "content",
+			},
 			want: "",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := GetMessageAPIVersion(tt.args.msg); got != tt.want {
+			if got := GetMessageAPIVersion(tt.msg); got != tt.want {
 				t.Errorf("GetMessageAPIVersion() = %v, want %v", got, tt.want)
 			}
 		})
@@ -62,34 +57,32 @@ func TestGetMessageResourceType(t *testing.T) {
 	}
 	tests := []struct {
 		name string
-		args args
+		msg  *beehiveModel.Message
 		want string
 	}{
 		{
 			name: "TestGetMessageResourceType(): Case 1: Content pod",
-			args: args{
-				msg: &beehiveModel.Message{
-					Content: &corev1.Pod{
-						TypeMeta: metav1.TypeMeta{
-							Kind:       "Pod",
-							APIVersion: "v1",
-						},
+			msg: &beehiveModel.Message{
+				Content: &corev1.Pod{
+					TypeMeta: metav1.TypeMeta{
+						Kind:       "Pod",
+						APIVersion: "v1",
 					},
-				}},
+				},
+			},
 			want: "Pod",
 		},
 		{
 			name: "TestGetMessageResourceType(): Case 2: Content other",
-			args: args{
-				msg: &beehiveModel.Message{
-					Content: "content",
-				}},
+			msg: &beehiveModel.Message{
+				Content: "content",
+			},
 			want: "",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := GetMessageResourceType(tt.args.msg); got != tt.want {
+			if got := GetMessageResourceType(tt.msg); got != tt.want {
 				t.Errorf("GetMessageResourceType() = %v, want %v", got, tt.want)
 			}
 		})
@@ -97,28 +90,25 @@ func TestGetMessageResourceType(t *testing.T) {
 }
 
 func TestSetMetaType(t *testing.T) {
-	type args struct {
-		obj runtime.Object
-	}
 	tests := []struct {
 		name    string
-		args    args
+		obj     runtime.Object
 		wantErr bool
 	}{
 		{
 			name:    "TestSetMetaType(): Case 1: Set MetaType success",
-			args:    args{obj: &appsv1.Deployment{}},
+			obj:     &appsv1.Deployment{},
 			wantErr: false,
 		},
 		{
 			name:    "TestSetMetaType(): Case 2: Set MetaType fail",
-			args:    args{obj: nil},
+			obj:     nil,
 			wantErr: true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := SetMetaType(tt.args.obj); (err != nil) != tt.wantErr {
+			if err := SetMetaType(tt.obj); (err != nil) != tt.wantErr {
 				t.Errorf("SetMetaType() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
@@ -126,38 +116,35 @@ func TestSetMetaType(t *testing.T) {
 }
 
 func TestUnsafeKindToResource(t *testing.T) {
-	type args struct {
-		k string
-	}
 	tests := []struct {
 		name string
-		args args
+		args string
 		want string
 	}{
 		{
 			name: "TestUnsafeKindToResource(): Case 1: Endpoints",
-			args: args{k: "Endpoints"},
+			args: "Endpoints",
 			want: "endpoints",
 		},
 		{
 			name: "TestUnsafeKindToResource(): Case 2: s",
-			args: args{k: "Class"},
+			args: "Class",
 			want: "classes",
 		},
 		{
 			name: "TestUnsafeKindToResource(): Case 3: y",
-			args: args{k: "Copy"},
+			args: "Copy",
 			want: "copies",
 		},
 		{
 			name: "TestUnsafeKindToResource(): Case 4: other",
-			args: args{k: "Service"},
+			args: "Service",
 			want: "services",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := UnsafeKindToResource(tt.args.k); got != tt.want {
+			if got := UnsafeKindToResource(tt.args); got != tt.want {
 				t.Errorf("UnsafeKindToResource() = %v, want %v", got, tt.want)
 			}
 		})
@@ -165,53 +152,50 @@ func TestUnsafeKindToResource(t *testing.T) {
 }
 
 func TestUnsafeResourceToKind(t *testing.T) {
-	type args struct {
-		r string
-	}
 	tests := []struct {
 		name string
-		args args
+		args string
 		want string
 	}{
 		{
 			name: "TestUnsafeResourceToKind(): Case 1: Endpoint",
-			args: args{r: "endpoints"},
+			args: "endpoints",
 			want: "Endpoints",
 		},
 		{
 			name: "TestUnsafeResourceToKind(): Case 2: Node",
-			args: args{r: "nodes"},
+			args: "nodes",
 			want: "Node",
 		},
 		{
 			name: "TestUnsafeResourceToKind(): Case 3: Service",
-			args: args{r: "services"},
+			args: "services",
 			want: "Service",
 		},
 		{
 			name: "TestUnsafeResourceToKind(): Case 4: ies",
-			args: args{r: "Copies"},
+			args: "Copies",
 			want: "Copy",
 		},
 		{
 			name: "TestUnsafeResourceToKind(): Case 5: es",
-			args: args{r: "Classes"},
+			args: "Classes",
 			want: "Class",
 		},
 		{
 			name: "TestUnsafeResourceToKind(): Case 6: s",
-			args: args{r: "Deployments"},
+			args: "Deployments",
 			want: "Deployment",
 		},
 		{
 			name: "TestUnsafeResourceToKind(): Case 7: other",
-			args: args{r: "Other"},
+			args: "Other",
 			want: "Other",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := UnsafeResourceToKind(tt.args.r); got != tt.want {
+			if got := UnsafeResourceToKind(tt.args); got != tt.want {
 				t.Errorf("UnsafeResourceToKind() = %v, want %v", got, tt.want)
 			}
 		})
@@ -242,19 +226,16 @@ func TestUnstructuredAttr(t *testing.T) {
 		"metadata.namespaces": "test",
 	})
 	_ = unstructured.SetNestedField(uns.Object, "node1", "spec", "nodeName")
-	type args struct {
-		obj runtime.Object
-	}
 	tests := []struct {
 		name    string
-		args    args
+		obj     runtime.Object
 		want    labels.Set
 		want1   fields.Set
 		wantErr bool
 	}{
 		{
 			name: "TestUnstructuredAttr(): Case 1: Deployment",
-			args: args{obj: dep},
+			obj:  dep,
 			want: dep.GetLabels(),
 			want1: map[string]string{
 				"metadata.name":      "deployment1",
@@ -264,7 +245,7 @@ func TestUnstructuredAttr(t *testing.T) {
 		},
 		{
 			name: "TestUnstructuredAttr(): Case 2: Pod",
-			args: args{obj: uns},
+			obj:  uns,
 			want: uns.GetLabels(),
 			want1: map[string]string{
 				"metadata.name":       "uns1",
@@ -276,7 +257,7 @@ func TestUnstructuredAttr(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, got1, err := UnstructuredAttr(tt.args.obj)
+			got, got1, err := UnstructuredAttr(tt.obj)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("UnstructuredAttr() error = %v, wantErr %v", err, tt.wantErr)
 				return
