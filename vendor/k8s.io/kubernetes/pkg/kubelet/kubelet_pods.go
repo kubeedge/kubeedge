499c99b3d7a5b4daef7c630c8ec6b873cdf698e8
wackxu <xushiwei5@huawei.com>
2023-02-24 15:10:14 +0800

    fix
    Signed-off-by: wackxu <xushiwei5@huawei.com>

--- vendor/k8s.io/kubernetes/pkg/kubelet/kubelet_pods.go
+++ vendor/k8s.io/kubernetes/pkg/kubelet/kubelet_pods.go
@@ -31,6 +31,7 @@ import (
 	"sort"
 	"strings"
 
+	"k8s.io/kubernetes/pkg/kubelet/envvars"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
