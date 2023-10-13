b2babe789bfeb20d4d1c90f67572028350268ec7
Shelley-BaoYue <baoyue2@huawei.com>
2023-10-13 14:23:50 +0800

    reclaim space for docker
    Signed-off-by: Shelley-BaoYue <baoyue2@huawei.com>

--- hack/make-rules/image.sh
+++ hack/make-rules/image.sh
@@ -38,7 +38,7 @@ ALL_IMAGES_AND_TARGETS=(
 
 GO_LDFLAGS="$(${KUBEEDGE_ROOT}/hack/make-rules/version.sh)"
 IMAGE_TAG=$(git describe --tags)
-
+DOCKER_BUILD_AND_SYSTEM_PRUNE=${DOCKER_BUILD_AND_SYSTEM_PRUNE:-"false"}
 
 function get_imagename_by_target() {
   local key=$1
@@ -88,6 +88,11 @@ function build_images() {
     set -x
     docker build --build-arg GO_LDFLAGS="${GO_LDFLAGS}" -t kubeedge/${IMAGE_NAME}:${IMAGE_TAG} -f ${DOCKERFILE_PATH} .
     set +x
+
+    if [[ "${DOCKER_BUILD_AND_SYSTEM_PRUNE}" = "true" ]]; then
+      docker builder prune -f
+      docker system prune -f
+    fi
   done
 }
 
