23194d5ad13fe69e9fdb73eb5b9f4b560086292f
wujunyi <wu65830600@163.com>
2023-10-27 01:23:22 +0800

    chore: update
    Signed-off-by: wujunyi <wu65830600@163.com>

--- keadm/cmd/keadm/app/cmd/util/nssm.go
+++ keadm/cmd/keadm/app/cmd/util/nssm.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2023 The KubeEdge Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package util
 
 import (
@@ -57,14 +73,13 @@ func InstallNSSMService(name, path string, args ...string) error {
 
 func IsNSSMServiceExist(service string) bool {
 	cmd := NewCommand("nssm status " + service)
-	_err := cmd.Exec()
-	return _err == nil
+	return cmd.Exec() == nil
 }
 
 func IsNSSMServiceRunning(service string) bool {
 	cmd := NewCommand("nssm status " + service)
-	_err := cmd.Exec()
-	if _err != nil || cmd.ExitCode > 0 {
+	err := cmd.Exec()
+	if err != nil || cmd.ExitCode > 0 {
 		return false
 	}
 	if strings.Contains(cmd.GetStdOut(), "SERVICE_RUNNING") {
