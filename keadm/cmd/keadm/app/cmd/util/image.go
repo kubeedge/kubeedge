1b6fa3643f2432472ad07e585ba1682a08f79651
luomengY <2938893385@qq.com>
2023-11-26 18:39:49 +0800

    Supports installing edgecore without installing the CNI plugin
    Signed-off-by: luomengY <2938893385@qq.com>

--- keadm/cmd/keadm/app/cmd/util/image.go
+++ keadm/cmd/keadm/app/cmd/util/image.go
@@ -280,7 +280,7 @@ func (runtime *CRIRuntime) CopyResources(edgeImage string, files map[string]stri
 		Linux: &runtimeapi.LinuxPodSandboxConfig{
 			SecurityContext: &runtimeapi.LinuxSandboxSecurityContext{
 				NamespaceOptions: &runtimeapi.NamespaceOption{
-					Network: runtimeapi.NamespaceMode_POD,
+					Network: runtimeapi.NamespaceMode_NODE,
 				},
 			},
 		},
