8bd1c1b4b2cb795131357eae68ca6ac0f96684da
WillardHu <wei.hu@daocloud.io>
2024-01-17 18:08:01 +0800

    Modify the command for upgrading edgenode
    Signed-off-by: WillardHu <wei.hu@daocloud.io>

--- keadm/cmd/keadm/app/cmd/cloud/upgrade.go
+++ keadm/cmd/keadm/app/cmd/cloud/upgrade.go
@@ -1,3 +1,18 @@
+/*
+Copyright 2024 The KubeEdge Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package cloud
 
 import (
@@ -13,7 +28,8 @@ func NewCloudUpgrade() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "cloud",
 		Short: "Upgrade the cloud components",
-		Long:  "Upgrade the cloud components to the desired version, it uses helm to upgrade the installed release of cloudcore.",
+		Long: "Upgrade the cloud components to the desired version, " +
+			"it uses helm to upgrade the installed release of cloudcore chart, which includes all the cloud components",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			tool := helm.NewCloudCoreHelmTool(opts.KubeConfig, opts.KubeEdgeVersion)
 			return tool.Upgrade(opts)
