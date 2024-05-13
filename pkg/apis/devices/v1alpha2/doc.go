4f50a5b63cff52d35ad4eef7ea75a7ee703e342e
侯玲玉 <houlingyu@macbook-pro-3.local>
2024-05-13 22:52:58 +0800

    add openapi-gen note
    Signed-off-by: lingyu.hou <lingyu.hou@daocloud.io>

--- pkg/apis/devices/v1alpha2/doc.go
+++ pkg/apis/devices/v1alpha2/doc.go
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
+// +k8s:openapi-gen=true
+// +groupName=devices.kubeedge.io
 // +k8s:deepcopy-gen=package
 package v1alpha2
