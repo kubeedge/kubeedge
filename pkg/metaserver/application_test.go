4f4ae94cd9d51a01a22ea23447cc60b2fb94c9d8
Shubham Singh <shubhammahar1306@gmail.com>
2024-06-20 12:38:27 +0000

    small mistakes
    Signed-off-by: GitHub <noreply@github.com>

--- pkg/metaserver/application_test.go
+++ pkg/metaserver/application_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 The KubeEdge Authors.
+Copyright 2024 The KubeEdge Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
