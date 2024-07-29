e201b4cf2bcd2f9a015ef2ed144288d2a42e4fd0
wbc6080 <wangbincheng4@huawei.com>
2024-07-29 17:06:27 +0800

    migrate kubeedge api to stage directory
    Signed-off-by: wbc6080 <wangbincheng4@huawei.com>

---
+++ staging/src/github.com/kubeedge/api/devices/v1alpha2/register.go
@@ -0,0 +1,84 @@
+/*
+Copyright 2019 The KubeEdge Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// NOTE: Boilerplate only.  Ignore this file.
+
+// Package v1alpha2 contains API Schema definitions for the devices v1alpha2 API group
+// +k8s:openapi-gen=true
+// +k8s:deepcopy-gen=package,register
+// +k8s:conversion-gen=kubeedge/cloud/pkg/apis/devices
+// +k8s:defaulter-gen=TypeMeta
+// +groupName=devices.kubeedge.io
+package v1alpha2
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+var (
+	SchemeBuilder      runtime.SchemeBuilder
+	localSchemeBuilder = &SchemeBuilder
+	AddToScheme        = localSchemeBuilder.AddToScheme
+)
+
+const (
+	// GroupName is the group name use in this package.
+	GroupName = "devices.kubeedge.io"
+	// Version is the API version.
+	Version = "v1alpha2"
+)
+
+var (
+	// SchemeGroupVersion is the group version used to register these objects.
+	SchemeGroupVersion = schema.GroupVersion{Group: GroupName, Version: Version}
+)
+
+func init() {
+	// We only register manually written functions here. The registration of the
+	// generated functions takes place in the generated files. The separation
+	// makes the code compile even when the generated files are missing.
+	localSchemeBuilder.Register(addKnownTypes)
+}
+
+// Resource takes an unqualified resource and returns a Group-qualified GroupResource.
+func Resource(resource string) schema.GroupResource {
+	return SchemeGroupVersion.WithResource(resource).GroupResource()
+}
+
+// addKnownTypes adds the set of types defined in this package to the supplied scheme.
+func addKnownTypes(scheme *runtime.Scheme) error {
+	scheme.AddKnownTypes(SchemeGroupVersion,
+		&Device{},
+		&DeviceList{},
+		&DeviceModel{},
+		&DeviceModelList{},
+	)
+	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
+	return nil
+}
+
+func AddDeviceCrds(scheme *runtime.Scheme) error {
+	// Add Device
+	scheme.AddKnownTypes(SchemeGroupVersion, &Device{}, &DeviceList{})
+	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
+	// Add DeviceModel
+	scheme.AddKnownTypes(SchemeGroupVersion, &DeviceModel{}, &DeviceModelList{})
+	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
+
+	return nil
+}
---
+++ staging/src/github.com/kubeedge/api/rules/v1/doc.go
@@ -0,0 +1,4 @@
+// +k8s:openapi-gen=true
+// +k8s:deepcopy-gen=package
+// +groupName=rules.kubeedge.io
+package v1
