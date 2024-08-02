098e1061140f08ade3793bdeb25b7056fe62282b
EraKin575 <tejaskumar574@gmail.com>
2024-08-02 10:05:41 +0530

    added verification for openapi-spec
    Signed-off-by: EraKin575 <tejaskumar574@gmail.com>
    
    added verification for openapi-spec
    
    Signed-off-by: EraKin575 <tejaskumar574@gmail.com>
    
    modified verify-apidoc.sh
    
    Signed-off-by: EraKin575 <tejaskumar574@gmail.com>
    
    modified makefile
    
    Signed-off-by: EraKin575 <tejaskumar574@gmail.com>
    
    added swagger docs
    
    Signed-off-by: EraKin575 <tejaskumar574@gmail.com>

---
+++ hack/verify-apidoc.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+# Copyright 2024 The KubeEdge Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+KUBEEDGE_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)"
+DIFFROOT="${KUBEEDGE_ROOT}/apidoc"
+TMP_DIFFROOT="${KUBEEDGE_ROOT}/_tmp/apidoc"
+_tmp="${KUBEEDGE_ROOT}/_tmp"
+
+cleanup() {
+  rm -rf "${_tmp}"
+}
+
+trap cleanup EXIT SIGINT
+
+cleanup
+
+mkdir -p "${TMP_DIFFROOT}"
+cp -a "${DIFFROOT}"/* "${TMP_DIFFROOT}"
+
+chmod +x "${KUBEEDGE_ROOT}/apidoc/tools/generate-openapi.sh"
+chmod +x "${KUBEEDGE_ROOT}/apidoc/tools/update-swagger-docs.sh"
+
+
+"${KUBEEDGE_ROOT}/apidoc/tools/generate-openapi.sh"
+"${KUBEEDGE_ROOT}/apidoc/tools/update-swagger-docs.sh"
+
+chmod -x "${KUBEEDGE_ROOT}/apidoc/tools/generate-openapi.sh"
+chmod -x "${KUBEEDGE_ROOT}/apidoc/tools/update-swagger-docs.sh"
+
+
+echo "Diffing ${DIFFROOT} against freshly generated definition"
+ret=0
+diff -Naupr "${DIFFROOT}" "${TMP_DIFFROOT}" || ret=$?
+cp -a "${TMP_DIFFROOT}"/* "${DIFFROOT}"
+
+if [[ $ret -eq 0 ]]; then
+  echo "${DIFFROOT} up to date."
+else
+  echo "${DIFFROOT} is out of date. Please re-generate swagger.json"
+  exit 1 
+
+fi
