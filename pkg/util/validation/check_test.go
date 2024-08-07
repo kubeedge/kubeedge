d91d405cef3cce85883a308746edbd136405b63c
Shubham Singh <shubhammahar1306@gmail.com>
2024-08-07 13:24:53 +0000

    standard lib -> testify/assert
    Signed-off-by: GitHub <noreply@github.com>

--- pkg/util/validation/check_test.go
+++ pkg/util/validation/check_test.go
@@ -4,22 +4,23 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestFileIsExist(t *testing.T) {
+	assert := assert.New(t)
+
 	dir := t.TempDir()
 
 	ef, err := os.CreateTemp(dir, "CheckFileIsExist")
-	if err == nil {
-		if !FileIsExist(ef.Name()) {
-			t.Fatalf("file %v should exist", ef.Name())
-		}
-	}
+	assert.NoError(err, "Error creating temporary file")
+	defer os.Remove(ef.Name())
+
+	assert.True(FileIsExist(ef.Name()), "file %v should exist", ef.Name())
 
 	nonexistentDir := filepath.Join(dir, "not_exist_dir")
 	notExistFile := filepath.Join(nonexistentDir, "not_exist_file")
 
-	if FileIsExist(notExistFile) {
-		t.Fatalf("file %v should not exist", notExistFile)
-	}
+	assert.False(FileIsExist(notExistFile), "file %v should not exist", notExistFile)
 }
