4b51d0f8daffec003fd640aa42b8db595ca5353b
luomengY <2938893385@qq.com>
2024-08-12 12:41:24 +0800

    Get the ConfigMap from the edge node.
    Signed-off-by: luomengY <2938893385@qq.com>

--- edge/pkg/metamanager/client/secret.go
+++ edge/pkg/metamanager/client/secret.go
@@ -9,6 +9,7 @@ import (
 	"github.com/kubeedge/beehive/pkg/core/model"
 	"github.com/kubeedge/kubeedge/edge/pkg/common/message"
 	"github.com/kubeedge/kubeedge/edge/pkg/common/modules"
+	"github.com/kubeedge/kubeedge/edge/pkg/metamanager/dao"
 )
 
 // SecretsGetter is interface to get client secrets
@@ -51,39 +52,36 @@ func (c *secrets) Delete(string) error {
 func (c *secrets) Get(name string) (*api.Secret, error) {
 	resource := fmt.Sprintf("%s/%s/%s", c.namespace, model.ResourceTypeSecret, name)
 	secretMsg := message.BuildMsg(modules.MetaGroup, "", modules.EdgedModuleName, resource, model.QueryOperation, nil)
-	msg, err := c.send.SendSync(secretMsg)
-	if err != nil {
-		return nil, fmt.Errorf("get secret from metaManager failed, err: %v", err)
-	}
-	errContent, ok := msg.GetContent().(error)
-	if ok {
-		return nil, errContent
-	}
-	content, err := msg.GetContentData()
-	if err != nil {
-		return nil, fmt.Errorf("parse message to secret failed, err: %v", err)
+	remoteGet := func() (*api.Secret, error) {
+		msg, err := c.send.SendSync(secretMsg)
+		if err != nil {
+			return nil, fmt.Errorf("get secret from metaManager failed, err: %v", err)
+		}
+		errContent, ok := msg.GetContent().(error)
+		if ok {
+			return nil, errContent
+		}
+		content, err := msg.GetContentData()
+		if err != nil {
+			return nil, fmt.Errorf("parse message to secret failed, err: %v", err)
+		}
+		return handleSecretFromMetaManager(content)
 	}
 
-	if msg.GetOperation() == model.ResponseOperation && msg.GetSource() == modules.MetaManagerModuleName {
-		return handleSecretFromMetaDB(content)
+	metas, err := dao.QueryMeta("key", resource)
+	if err != nil || len(*metas) == 0 {
+		return remoteGet()
 	}
-	//else
-	return handleSecretFromMetaManager(content)
+	return handleSecretFromMetaDB(metas)
 }
 
-func handleSecretFromMetaDB(content []byte) (*api.Secret, error) {
-	var lists []string
-	err := json.Unmarshal(content, &lists)
-	if err != nil {
-		return nil, fmt.Errorf("unmarshal message to secret list from db failed, err: %v", err)
-	}
-
-	if len(lists) != 1 {
-		return nil, fmt.Errorf("secret length from meta db is %d", len(lists))
+func handleSecretFromMetaDB(lists *[]string) (*api.Secret, error) {
+	if len(*lists) != 1 {
+		return nil, fmt.Errorf("secret length from meta db is %d", len(*lists))
 	}
 
 	var secret api.Secret
-	err = json.Unmarshal([]byte(lists[0]), &secret)
+	err := json.Unmarshal([]byte((*lists)[0]), &secret)
 	if err != nil {
 		return nil, fmt.Errorf("unmarshal message to secret from db failed, err: %v", err)
 	}
