e4ded69369f9310224d98578cbadbe6b9e303b91
vivekbisen04 <vivekbisen204@gmail.com>
2025-02-05 15:52:58 +0530

    errorf to error
    Signed-off-by: vivekbisen04 <vivekbisen204@gmail.com>

--- pkg/features/features_test.go
+++ pkg/features/features_test.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2025 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package features
 
 import (
@@ -9,13 +25,13 @@ import (
 func TestDefaultFeatureGates(t *testing.T) {
 	// First verify initialization
 	if DefaultMutableFeatureGate == nil {
-		t.Errorf("DefaultMutableFeatureGate should not be nil")
+		t.Error("DefaultMutableFeatureGate should not be nil")
 	}
 	if DefaultFeatureGate == nil {
-		t.Errorf("DefaultFeatureGate should not be nil")
+		t.Error("DefaultFeatureGate should not be nil")
 	}
 	if DefaultFeatureGate != DefaultMutableFeatureGate {
-		t.Errorf("DefaultFeatureGate should be the same as DefaultMutableFeatureGate")
+		t.Error("DefaultFeatureGate should be the same as DefaultMutableFeatureGate")
 	}
 
 	testCases := []struct {
@@ -67,14 +83,14 @@ func TestFeatureGateSpecifications(t *testing.T) {
 		{
 			feature: RequireAuthorization,
 			expectedSpec: featuregate.FeatureSpec{
-				Default:     false,
+				Default:    false,
 				PreRelease: featuregate.Alpha,
 			},
 		},
 		{
 			feature: ModuleRestart,
 			expectedSpec: featuregate.FeatureSpec{
-				Default:     false,
+				Default:    false,
 				PreRelease: featuregate.Alpha,
 			},
 		},
