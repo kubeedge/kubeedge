b3d8fbbd120e14c16a652acf1f8848808f97f471
Dhiren-Mhatre <kp064669@gmail.com>
2025-03-12 19:19:24 +0530

    [test]:improve coverage of containerexec_connection_test.go in cloud/pkg/cloudstream
    Signed-off-by: Dhiren-Mhatre <kp064669@gmail.com>

--- cloud/pkg/cloudstream/containerexec_connection_test.go
+++ cloud/pkg/cloudstream/containerexec_connection_test.go
@@ -17,9 +17,12 @@ limitations under the License.
 package cloudstream
 
 import (
+	"context"
+	"errors"
 	"net/http"
 	"net/url"
 	"testing"
+	"time"
 
 	"github.com/emicklei/go-restful"
 	"github.com/stretchr/testify/assert"
@@ -81,16 +84,19 @@ func TestSetEdgePeerDone_Exec(t *testing.T) {
 		closeChan:    make(chan bool),
 	}
 
+	received := make(chan bool, 1)
 	go func() {
-		execConn.SetEdgePeerDone()
+		select {
+		case <-execConn.edgePeerStop:
+			received <- true
+		case <-time.After(100 * time.Millisecond):
+			received <- false
+		}
 	}()
 
-	select {
-	case <-execConn.edgePeerStop:
-		assert.True(true)
-	case <-execConn.closeChan:
-		assert.Fail("Expected edgePeerStop to receive but got closeChan")
-	}
+	execConn.SetEdgePeerDone()
+
+	assert.True(<-received, "edgePeerStop channel should receive a signal")
 }
 
 func TestEdgePeerDone_Exec(t *testing.T) {
@@ -163,4 +169,186 @@ func TestSendConnection_Exec(t *testing.T) {
 	assert.Equal(mockTunneler.lastMessage.MessageType, stream.MessageTypeExecConnect)
 	expectedData, _ := edgedConnector.CreateConnectMessage()
 	assert.Equal(mockTunneler.lastMessage.Data, expectedData.Data)
+}
+
+func TestServe_Exec(t *testing.T) {
+	testCases := []struct {
+		name          string
+		setup         func(t *testing.T) (*ContainerExecConnection, context.CancelFunc)
+		expectedError bool
+		errorContains string
+	}{
+		{
+			name: "Context done",
+			setup: func(t *testing.T) (*ContainerExecConnection, context.CancelFunc) {
+				conn := &MockConn{}
+				conn.readBuffer.Write([]byte("test data"))
+
+				tun := &MockTunneler{}
+
+				ctx, cancel := context.WithCancel(context.Background())
+
+				session := &Session{
+					tunnel: tun,
+				}
+
+				r := &restful.Request{
+					Request: &http.Request{
+						Method: "GET",
+						URL:    &url.URL{},
+						Header: http.Header{},
+					},
+				}
+
+				execConn := &ContainerExecConnection{
+					MessageID:    1,
+					ctx:          ctx,
+					r:            r,
+					Conn:         conn,
+					session:      session,
+					edgePeerStop: make(chan struct{}),
+					closeChan:    make(chan bool),
+				}
+
+				cancel()
+
+				return execConn, cancel
+			},
+			expectedError: false,
+		},
+		{
+			name: "Edge peer done",
+			setup: func(t *testing.T) (*ContainerExecConnection, context.CancelFunc) {
+				conn := &MockConn{}
+				conn.readBuffer.Write([]byte("test data"))
+
+				tun := &MockTunneler{}
+
+				ctx, cancel := context.WithCancel(context.Background())
+
+				session := &Session{
+					tunnel: tun,
+				}
+
+				r := &restful.Request{
+					Request: &http.Request{
+						Method: "GET",
+						URL:    &url.URL{},
+						Header: http.Header{},
+					},
+				}
+
+				execConn := &ContainerExecConnection{
+					MessageID:    1,
+					ctx:          ctx,
+					r:            r,
+					Conn:         conn,
+					session:      session,
+					edgePeerStop: make(chan struct{}),
+					closeChan:    make(chan bool),
+				}
+
+				go func() {
+					time.Sleep(50 * time.Millisecond)
+					execConn.EdgePeerDone() <- struct{}{}
+				}()
+
+				return execConn, cancel
+			},
+			expectedError: true,
+			errorContains: "find edge peer done",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			assert := assert.New(t)
+
+			execConn, cancel := tc.setup(t)
+			defer cancel()
+
+			done := make(chan error)
+
+			go func() {
+				err := execConn.Serve()
+				done <- err
+			}()
+
+			var err error
+			select {
+			case err = <-done:
+			case <-time.After(500 * time.Millisecond):
+				cancel()
+				err = <-done
+			}
+
+			if tc.expectedError {
+				assert.Error(err)
+				if tc.errorContains != "" {
+					assert.Contains(err.Error(), tc.errorContains)
+				}
+			} else {
+				if err != nil {
+					t.Logf("Expected no error but got: %v. This is acceptable in this test case.", err)
+				}
+			}
+		})
+	}
+}
+
+func TestSetEdgePeerDone_Closed(t *testing.T) {
+	assert := assert.New(t)
+
+	execConn := &ContainerExecConnection{
+		MessageID:    1,
+		edgePeerStop: make(chan struct{}),
+		closeChan:    make(chan bool),
+	}
+
+	close(execConn.closeChan)
+
+	received := make(chan bool, 1)
+	go func() {
+		select {
+		case <-execConn.edgePeerStop:
+			received <- true
+		case <-time.After(100 * time.Millisecond):
+			received <- false
+		}
+	}()
+
+	execConn.SetEdgePeerDone()
+
+	assert.False(<-received, "edgePeerStop channel should not receive a signal when closeChan is closed")
+}
+
+func TestSendConnection_WriteToTunnelError(t *testing.T) {
+	assert := assert.New(t)
+
+	mockConn := &MockConn{}
+	mockTunneler := &MockTunneler{
+		err: errors.New("write error"),
+	}
+	session := &Session{
+		tunnel: mockTunneler,
+	}
+	r := &restful.Request{
+		Request: &http.Request{
+			Method: "GET",
+			URL:    &url.URL{},
+			Header: http.Header{},
+		},
+	}
+
+	execConn := &ContainerExecConnection{
+		MessageID: 1,
+		r:         r,
+		Conn:      mockConn,
+		session:   session,
+	}
+
+	connector, err := execConn.SendConnection()
+	assert.Error(err)
+	assert.Contains(err.Error(), "write error")
+	assert.Nil(connector)
 }
