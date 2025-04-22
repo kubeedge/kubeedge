6f259961ae4efcdd96188b0a59abd06d2715e300
Shelley-BaoYue <baoyue2@huawei.com>
2025-04-22 09:27:59 +0800

    generate code and crds for ConfigUpdateJob API
    Signed-off-by: Shelley-BaoYue <baoyue2@huawei.com>

--- hack/local-up-kubeedge.sh
+++ hack/local-up-kubeedge.sh
@@ -185,6 +185,7 @@ function create_operation_crd {
   echo "creating the operation crd..."
   kubectl apply -f ${KUBEEDGE_ROOT}/build/crds/operations/operations_v1alpha2_nodeupgradejob.yaml
   kubectl apply -f ${KUBEEDGE_ROOT}/build/crds/operations/operations_v1alpha2_imageprepulljob.yaml
+  kubectl apply -f ${KUBEEDGE_ROOT}/build/crds/operations/operations_v1alpha2_configupdatejob.yaml
 }
 
 function create_serviceaccountaccess_crd {
