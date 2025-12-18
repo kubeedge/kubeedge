c166da21b3697fa491d755516d8e13f8536a5a13
ddc-baiye <dongdong.chen@bluedotai.cn>
2025-12-18 16:44:43 +0800

    Add test for PrintDefaultConfigAndExitIfRequested
    Signed-off-by: ddc-baiye <dongdong.chen@bluedotai.cn>

--- pkg/util/flag/flags_test.go
+++ pkg/util/flag/flags_test.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2024 The KubeEdge Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package flag
 
 import (
@@ -83,8 +99,8 @@ func TestConfigValue_Set(t *testing.T) {
 				assert.Error(t, err)
 			} else {
 				assert.NoError(t, err)
-				assert.Equal(t, tt.wantValue, cv)
 			}
+			assert.Equal(t, tt.wantValue, cv)
 		})
 	}
 }
@@ -151,5 +167,23 @@ func TestPrintMinConfigAndExitIfRequested(t *testing.T) {
 
 	assert.NotPanics(t, func() {
 		PrintMinConfigAndExitIfRequested(config)
+	})
+}
+
+func TestPrintDefaultConfigAndExitIfRequested(t *testing.T) {
+	// This function calls os.Exit(1) or fmt.Println, which is hard to test directly.
+	// However, we can test the case where defaultConfigFlag is False (default),
+	// ensuring it does NOT panic or exit.
+
+	// Save original flag value
+	original := *defaultConfigFlag
+	defer func() { *defaultConfigFlag = original }()
+
+	// Test case 1: defaultConfigFlag is False (should just return)
+	*defaultConfigFlag = ConfigFalse
+	config := map[string]string{"key": "value"}
+
+	assert.NotPanics(t, func() {
+		PrintDefaultConfigAndExitIfRequested(config)
 	})
 }
