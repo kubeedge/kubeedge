15103942678474ecacadfab98bc726901f45e6b7
Shelley-BaoYue <baoyue2@huawei.com>
2026-01-26 17:38:54 +0800

    fix transaction in device DB
    Signed-off-by: Shelley-BaoYue <baoyue2@huawei.com>

--- edge/pkg/metamanager/dao/dbclient/device.go
+++ edge/pkg/metamanager/dao/dbclient/device.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2025 The KubeEdge Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package dbclient
 
 import (
@@ -18,7 +34,7 @@ func NewDeviceService() *DeviceService {
 
 // SaveDevice saves a device in a transaction
 func (s *DeviceService) SaveDevice(doc *models.Device) error {
-	tx := s.db
+	tx := s.db.Begin()
 	if err := tx.Create(doc).Error; err != nil {
 		tx.Rollback()
 		klog.Errorf("Failed to insert Device data: %v", err)
@@ -86,7 +102,7 @@ func (s *DeviceService) UpdateDeviceMulti(updates []models.DeviceUpdate) error {
 
 // AddDeviceTrans handles transactional creation of devices, attributes, and twins
 func (s *DeviceService) AddDeviceTrans(adds []models.Device, addAttrs []models.DeviceAttr, addTwins []models.DeviceTwin) error {
-	tx := s.db
+	tx := s.db.Begin()
 
 	for _, device := range adds {
 		if err := tx.Create(&device).Error; err != nil {
@@ -117,19 +133,23 @@ func (s *DeviceService) AddDeviceTrans(adds []models.Device, addAttrs []models.D
 
 // DeleteDeviceTrans handles transactional deletion of devices, attributes, and twins
 func (s *DeviceService) DeleteDeviceTrans(deletes []string) error {
-	tx := s.db
+	tx := s.db.Begin()
 
 	for _, id := range deletes {
 		if err := tx.Delete(&models.Device{}, "id = ?", id).Error; err != nil {
 			tx.Rollback()
 			return err
 		}
-		if err := s.DeleteDeviceAttrByDeviceID(id); err != nil {
+
+		if err := tx.Where("deviceid = ?", id).Delete(&models.DeviceAttr{}).Error; err != nil {
 			tx.Rollback()
+			klog.Errorf("Failed to delete DeviceAttr by deviceID: %v", err)
 			return err
 		}
-		if err := s.DeleteDeviceTwinByDeviceID(id); err != nil {
+
+		if err := tx.Where("deviceid = ?", id).Delete(&models.DeviceTwin{}).Error; err != nil {
 			tx.Rollback()
+			klog.Errorf("Failed to delete DeviceTwin by deviceID: %v", err)
 			return err
 		}
 	}
@@ -144,16 +164,6 @@ func (s *DeviceService) SaveDeviceAttr(doc *models.DeviceAttr) error {
 			return err
 		}
 		klog.V(4).Info("Inserted DeviceAttr successfully")
-		return nil
-	})
-}
-
-func (s *DeviceService) DeleteDeviceAttrByDeviceID(deviceID string) error {
-	return s.db.Transaction(func(tx *gorm.DB) error {
-		if err := tx.Where("deviceid = ?", deviceID).Delete(&models.DeviceAttr{}).Error; err != nil {
-			klog.Errorf("Failed to delete DeviceAttr by deviceID: %v", err)
-			return err
-		}
 		return nil
 	})
 }
@@ -227,16 +237,6 @@ func (s *DeviceService) SaveDeviceTwin(doc *models.DeviceTwin) error {
 			return err
 		}
 		klog.V(4).Info("Inserted DeviceTwin successfully")
-		return nil
-	})
-}
-
-func (s *DeviceService) DeleteDeviceTwinByDeviceID(deviceID string) error {
-	return s.db.Transaction(func(tx *gorm.DB) error {
-		if err := tx.Where("deviceid = ?", deviceID).Delete(&models.DeviceTwin{}).Error; err != nil {
-			klog.Errorf("Failed to delete DeviceTwin by deviceID: %v", err)
-			return err
-		}
 		return nil
 	})
 }
