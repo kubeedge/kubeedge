578509823652bd7dc3a5a4d8fc639c9db6bd8d0c
Priyanshu6968 <priyanshunigam469@gmail.com>
2026-06-30 11:18:45 +0530

    pkg/security/certs: use t.TempDir in ReadPEMFile test
    Signed-off-by: Priyanshu6968 <priyanshunigam469@gmail.com>

--- pkg/security/certs/io_test.go
+++ pkg/security/certs/io_test.go
@@ -2,6 +2,7 @@ package certs
 
 import (
 	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -24,18 +25,11 @@ func TestReadWrite(t *testing.T) {
 }
 
 func TestReadPEMFileNoBlock(t *testing.T) {
-	file := "./testdata/ca/invalid.crt"
-	if err := os.MkdirAll("./testdata/ca", 0700); err != nil {
-		t.Fatal(err)
-	}
+	file := filepath.Join(t.TempDir(), "invalid.crt")
 	if err := os.WriteFile(file, []byte("not a pem block"), 0600); err != nil {
 		t.Fatal(err)
 	}
 	if _, err := ReadPEMFile(file); err == nil {
 		t.Fatal("expected error when file contains no PEM block, got nil")
-	}
-	// Clean
-	if err := os.RemoveAll("testdata"); err != nil {
-		t.Fatalf("failed to clean testdata, err: %v", err)
 	}
 }
