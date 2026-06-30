823de877baedf90c49e718de9a61f5be999711d0
Priyanshu6968 <priyanshunigam469@gmail.com>
2026-06-30 11:03:45 +0530

    pkg/security/certs: return error from ReadPEMFile when no PEM block is found
    Signed-off-by: Priyanshu6968 <priyanshunigam469@gmail.com>

--- pkg/security/certs/io_test.go
+++ pkg/security/certs/io_test.go
@@ -22,3 +22,20 @@ func TestReadWrite(t *testing.T) {
 		t.Fatalf("failed to clean testdata, err: %v", err)
 	}
 }
+
+func TestReadPEMFileNoBlock(t *testing.T) {
+	file := "./testdata/ca/invalid.crt"
+	if err := os.MkdirAll("./testdata/ca", 0700); err != nil {
+		t.Fatal(err)
+	}
+	if err := os.WriteFile(file, []byte("not a pem block"), 0600); err != nil {
+		t.Fatal(err)
+	}
+	if _, err := ReadPEMFile(file); err == nil {
+		t.Fatal("expected error when file contains no PEM block, got nil")
+	}
+	// Clean
+	if err := os.RemoveAll("testdata"); err != nil {
+		t.Fatalf("failed to clean testdata, err: %v", err)
+	}
+}
