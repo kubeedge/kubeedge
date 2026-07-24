a6f0756a5bec0ede232518533858c886af2a6b28
Norway-02 <anshulkhetade02@gmail.com>
2026-07-24 09:52:20 +0530

    docs(cloud/devicecontroller): add godoc comments to exported identifiers
    Signed-off-by: Norway-02 <anshulkhetade02@gmail.com>

--- cloud/pkg/devicecontroller/config/config.go
+++ cloud/pkg/devicecontroller/config/config.go
@@ -9,10 +9,13 @@ import (
 var Config Configure
 var once sync.Once
 
+// Configure holds the configuration for the devicecontroller module.
 type Configure struct {
 	v1alpha1.DeviceController
 }
 
+// InitConfigure initializes the global Config variable based on the provided
+// DeviceController configuration. It is safe to call multiple times.
 func InitConfigure(dc *v1alpha1.DeviceController) {
 	once.Do(func() {
 		Config = Configure{
