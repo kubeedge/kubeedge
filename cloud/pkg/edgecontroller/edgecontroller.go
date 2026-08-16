8cb014a9c67adac32887407fe755d90c67922787
Ankita Salaria <255383093+Ankitavasudev@users.noreply.github.com>
2026-08-17 02:39:40 +0530

    fix: replace klog.Exit with panic in edgecontroller.go
    Signed-off-by: Ankita Salaria <255383093+Ankitavasudev@users.noreply.github.com>

--- cloud/pkg/edgecontroller/edgecontroller.go
+++ cloud/pkg/edgecontroller/edgecontroller.go
@@ -1,7 +1,7 @@
 package edgecontroller
 
 import (
-	"k8s.io/klog/v2"
+	"fmt"`n`n`t"k8s.io/klog/v2"
 
 	"github.com/kubeedge/api/apis/componentconfig/cloudcore/v1alpha1"
 	"github.com/kubeedge/beehive/pkg/core"
